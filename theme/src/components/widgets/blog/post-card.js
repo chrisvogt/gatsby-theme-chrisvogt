f8fe846d102ca54fe17f691ca8d8a327f6c27034
Chris Vogt <mail@chrisvogt.me>
2019-11-19 19:37:41 -0800

    embedded youtube post remove

--- theme/src/components/widgets/blog/post-card.js
+++ theme/src/components/widgets/blog/post-card.js
@@ -7,25 +7,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default ({ category, date, excerpt, link, title }) => (
   <Link sx={{ variant: `styles.PostCard` }} to={link}>
-    <div sx={{ textAlign: `right` }}>
-      <span
-        sx={{
-          backgroundColor: `dark`,
-          color: `light`,
-          fontSize: 0,
-          py: 1,
-          px: 2,
-          borderRadius: `4px`
-        }}
-      >
-        {category}
-      </span>
-    </div>
     <Styled.h4>{title}</Styled.h4>
     <div>
       <FontAwesomeIcon icon={faCalendarAlt} /> <span>{date}</span>
     </div>
-    <p>{excerpt}</p>
+    <p>
+      <span
+        sx={{
+          backgroundColor: `muted`,
+          color: `text`,
+          fontSize: 0,
+          py: 1,
+          px: 2,
+          borderRadius: `2px`
+        }}
+      >
+        {category}
+      </span>{' '}
+      {excerpt}
+    </p>
     <div style={{ textAlign: `right` }}>
       <span>View post &raquo;</span>
     </div>
