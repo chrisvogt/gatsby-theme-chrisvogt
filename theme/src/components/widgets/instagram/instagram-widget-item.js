174596c60646c386352e0dbe0022306d7fe6bc28
Chris Vogt <mail@chrisvogt.me>
2020-05-09 16:21:00 -0700

    feat: add post metrics and type indicators to Instagram (#30)

---
+++ theme/src/components/widgets/instagram/instagram-widget-item.js
@@ -0,0 +1,120 @@
+/** @jsx jsx */
+import { jsx, Styled } from 'theme-ui'
+import { useState } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment, faHeart, faImages } from '@fortawesome/free-solid-svg-icons'
+
+import LazyLoad from '../../lazy-load'
+
+const InstagramWidgetItem = props => {
+  const {
+    post: {
+      id,
+      comments: { count: commentsCount = 0 },
+      images: { standard_resolution: { height, width, url } = {} },
+      likes: { count: likesCount = 0 },
+      link,
+      type
+    } = []
+  } = props
+
+  const [isInFocus, setIsInFocus] = useState(false)
+  const isCarousel = type === 'carousel'
+
+  return (
+    <Styled.a
+      key={id}
+      href={link}
+      onMouseEnter={() => setIsInFocus(true)}
+      onMouseLeave={() => setIsInFocus(false)}
+      style={{ lineHeight: 0 }}
+      title='Access media on Instagram'
+      rel='noopener noreferrer'
+      sx={{
+        variant: `styles.InstagramCard`
+      }}
+    >
+      {isInFocus && (
+        <div
+          className='instagram-item-overlay'
+          sx={{
+            color: `white`,
+            display: `flex`,
+            alignItems: `center`,
+            justifyContent: `space-evenly`,
+            background: `rgba(0, 0, 0, 0.3)`,
+            width: `100%`,
+            height: `100%`,
+            position: `absolute`,
+            zIndex: 33
+          }}
+        >
+          <ul
+            sx={{
+              alignItems: `center`,
+              color: `white`,
+              display: `flex`,
+              fontWeight: `bold`,
+              justifyContent: `center`,
+              listStyle: `none`,
+              p: 0
+            }}
+          >
+            <li
+              sx={{
+                alignItems: `center`,
+                mr: 4
+              }}
+            >
+              <span sx={{ mr: 1 }}>
+                <FontAwesomeIcon icon={faHeart} />{' '}
+              </span>
+              <span>{likesCount}</span>
+            </li>
+            <li
+              sx={{
+                mr: 0
+              }}
+            >
+              <span sx={{ mr: 1 }}>
+                <FontAwesomeIcon icon={faComment} />{' '}
+              </span>
+              <span>{commentsCount}</span>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      {isCarousel && (
+        <div
+          sx={{
+            color: `white`,
+            position: `absolute`,
+            top: 2,
+            right: 2
+          }}
+        >
+          <FontAwesomeIcon icon={faImages} />
+        </div>
+      )}
+
+      <LazyLoad>
+        <img
+          crossOrigin='anonymous'
+          src={url}
+          height={height}
+          width={width}
+          alt='Instagram post thumbnail'
+          sx={{
+            width: '100%',
+            height: '100%',
+            objectFit: 'cover'
+          }}
+        />
+      </LazyLoad>
+    </Styled.a>
+  )
+}
+
+export default InstagramWidgetItem
--- theme/src/components/widgets/instagram/instagram-widget.js
+++ theme/src/components/widgets/instagram/instagram-widget.js
@@ -1,22 +1,38 @@
 /** @jsx jsx */
-import { jsx, Styled } from 'theme-ui'
+import { jsx } from 'theme-ui'
 import { Grid } from '@theme-ui/components'
+import ReactPlaceholder from 'react-placeholder'
+import { RectShape } from 'react-placeholder/lib/placeholders'
+import { Grid as GridSpinner } from 'svg-loaders-react'
 
 import { getInstagramUsername } from '../../../selectors/metadata'
 import useInstagramPosts from '../../../hooks/use-instagram-posts'
 import useSiteMetadata from '../../../hooks/use-site-metadata'
 
 import CallToAction from '../call-to-action'
-import LazyLoad from '../../lazy-load'
 import Widget from '../widget'
+import WidgetItem from './instagram-widget-item'
 import WidgetHeader from '../widget-header'
+
+const ItemPlaceholder = (
+  <div className='image-placeholder'>
+    <RectShape color='#cecece' style={{ width: `100%`, minHeight: `330px` }} />
+  </div>
+)
 
 export default () => {
   const { isLoading, posts } = useInstagramPosts()
   const metadata = useSiteMetadata()
   const instagramUsername = getInstagramUsername(metadata)
 
-  const callToAction = (
+  const callToAction = isLoading ? (
+    <GridSpinner
+      fill='#1E90FF'
+      width='24'
+      height='24'
+      sx={{ verticalAlign: `middle` }}
+    />
+  ) : (
     <CallToAction
       title={`${instagramUsername} on Instagram`}
       url={`https://www.instagram.com/${instagramUsername}`}
@@ -31,50 +47,22 @@ export default () => {
       <WidgetHeader aside={callToAction}>Instagram</WidgetHeader>
 
       <div className='gallery'>
-        {isLoading && <h3>Loading...</h3>}
-
         <Grid
           sx={{
             gridGap: [3, 3, 3, 4],
             gridTemplateColumns: ['repeat(2, 1fr)', 'repeat(4, 1fr)']
           }}
         >
-          {!isLoading &&
-            posts.slice(0, 4).map(post => {
-              const {
-                id,
-                images: { standard_resolution: { height, width, url } = {} },
-                link
-              } = post
-              return (
-                <Styled.a
-                  key={id}
-                  href={link}
-                  style={{ lineHeight: 0 }}
-                  target='_blank'
-                  title='View image on Instagram'
-                  rel='noopener noreferrer'
-                  sx={{
-                    variant: `styles.InstagramCard`
-                  }}
-                >
-                  <LazyLoad>
-                    <img
-                      crossOrigin='anonymous'
-                      src={url}
-                      height={height}
-                      width={width}
-                      alt='Instagram post'
-                      sx={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'cover'
-                      }}
-                    />
-                  </LazyLoad>
-                </Styled.a>
-              )
-            })}
+          {(isLoading ? Array(4).fill() : posts).slice(0, 4).map(post => (
+            <ReactPlaceholder
+              customPlaceholder={ItemPlaceholder}
+              showLoadingAnimation
+              ready={!isLoading}
+              type='rect'
+            >
+              <WidgetItem post={post} />
+            </ReactPlaceholder>
+          ))}
         </Grid>
       </div>
     </Widget>
