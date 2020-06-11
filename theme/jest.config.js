e6aebc06561911eda9775e0cbbc74d2af4eaa8ce
Chris Vogt <mail@chrisvogt.me>
2020-06-10 23:45:08 -0700

    WIP: work on call to action test

--- theme/jest.config.js
+++ theme/jest.config.js
@@ -11,11 +11,16 @@ module.exports = {
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
+  globals: {
+    __PATH_PREFIX__: ``
+  },
+
   // An array of file extensions your modules use
   moduleFileExtensions: ['js', 'json', 'jsx'],
 
   moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'babel-jest'
+    '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`
   },
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
@@ -28,13 +33,17 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   testURL: 'http://localhost',
 
+  transform: {
+    '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`
+  },
+
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
 
   // Indicates whether each individual test should be reported during the run
   verbose: false
--- theme/src/components/widgets/call-to-action.spec.js
+++ theme/src/components/widgets/call-to-action.spec.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { Bars } from 'svg-loaders-react'
 import CallToAction from './call-to-action'
 
 describe('CallToAction', () => {
+  const title = 'Example Widget Title'
+
+  global.___loader = {
+    // eslint-disable-line no-underscore-dangle
+    enqueue: jest.fn()
+  }
+
   it('renders correctly', () => {
-    const title = 'Example Widget Title'
     const tree = renderer
       .create(
         <CallToAction title={title} isLoading={false}>
@@ -13,5 +20,53 @@ describe('CallToAction', () => {
       )
       .toJSON()
     expect(tree).toMatchSnapshot()
+  })
+
+  describe('loading indicator', () => {
+    it("doesn't a loading indicator by default", () => {
+      const testRenderer = renderer.create(
+        <CallToAction title={title}>Test</CallToAction>
+      )
+      const testInstance = testRenderer.root
+      expect(testInstance.findAllByType(Bars).length).toBe(0)
+    })
+
+    it('renders a loading indicator', () => {
+      const testRenderer = renderer.create(
+        <CallToAction title={title} isLoading>
+          Test
+        </CallToAction>
+      )
+      const testInstance = testRenderer.root
+      expect(testInstance.findAllByType(Bars).length).toBe(1)
+    })
+  })
+
+  describe('hyperlink vs Gatsby Router links', () => {
+    it('renders a hyperlink by default', () => {
+      const href = 'https://fake-link.com/my-profile'
+      const testRenderer = renderer.create(
+        <CallToAction title='My Profile' url={href}>
+          Visit profile
+        </CallToAction>
+      )
+      const testInstance = testRenderer.root
+      const hyperlinkEl = testInstance.findByType('a')
+      expect(!!hyperlinkEl.props.onClick).toBe(false)
+      expect(hyperlinkEl.props.href).toBe(href)
+    })
+
+    it('renders a Gatsby Router link', () => {
+      const route = '/about-me'
+      const testRenderer = renderer.create(
+        <CallToAction title={title} to={route}>
+          Learn more about me
+        </CallToAction>
+      )
+      const testInstance = testRenderer.root
+      const hyperlinkEl = testInstance.findByType('a')
+      expect(!!hyperlinkEl.props.onClick).toBe(true)
+      expect(hyperlinkEl.props.href).toBe(route)
+    })
   })
 })
