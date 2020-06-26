2297f97d35fb0e07727b2ba140a826e4cd40de79
Chris Vogt <mail@chrisvogt.me>
2020-06-26 00:37:17 -0700

    WIP: remove pinned item repository avatar from

--- theme/src/components/widgets/github/renderers/repository.js
+++ theme/src/components/widgets/github/renderers/repository.js
@@ -5,12 +5,10 @@ import ago from 's-ago'
 import PropTypes from 'prop-types'
 
 import CardFooter from '../../card-footer'
-import ViewExternal from '../../view-external'
 
 const Repository = ({
   description,
   nameWithOwner,
-  openGraphImageUrl,
   primaryLanguage,
   updatedAt
 }) => (
@@ -28,42 +26,29 @@ const Repository = ({
       }}
     >
       <Flex sx={{ flexGrow: 1, flexDirection: `column`, alignSelf: `center` }}>
-        <Heading as='h4' sx={{ pt: 0, pr: 3, pb: 0, mb: 0 }}>
+        <Heading as='h4' sx={{ pt: 0, pr: 3, pb: 0, mb: 1 }}>
           {nameWithOwner}
         </Heading>
         <span sx={{ color: `textMuted`, fontSize: 0, p: 0 }}>
           Updated {ago(new Date(updatedAt))}
         </span>
       </Flex>
-      <Flex sx={{ flexDirection: `column`, alignItems: `center` }}>
-        <img
-          alt='repository avatar'
-          height='42'
-          src={openGraphImageUrl}
-          sx={{
-            backgroundColor: `colors.background`,
-            borderRadius: `3px`
-          }}
-          width='42'
-        />
-      </Flex>
     </Flex>
 
     <span sx={{ flexGrow: 1 }}>{description}</span>
 
     <CardFooter>
-      {/* <ViewExternal platform='GitHub' /> */}
-      <span>
-      {primaryLanguage.name}
-      </span>
-      <span sx={{
-        backgroundColor: primaryLanguage.color,
-        ml: 2,
-        mb: `2px`,
-        width: `15px`,
-        height: `15px`,
-        borderRadius: `50%`
-      }}></span>
+      <span>{primaryLanguage.name}</span>
+      <span
+        sx={{
+          backgroundColor: primaryLanguage.color,
+          ml: 2,
+          mb: `2px`,
+          width: `15px`,
+          height: `15px`,
+          borderRadius: `50%`
+        }}
+      ></span>
     </CardFooter>
   </div>
 )
@@ -73,8 +58,6 @@ Repository.propTypes = {
   description: PropTypes.string.isRequired,
   /** @prop {String} nameWithOwner The repository's name with owner. */
   nameWithOwner: PropTypes.string.isRequired,
-  /** @prop {String} openGraphImageUrl The image used to represent this repository in Open Graph data. */
-  openGraphImageUrl: PropTypes.string.isRequired,
   /** @prop {String} updatedAt Identifies the date and time when the object was last updated. ISO-8601 encoded. */
   updatedAt: PropTypes.string.isRequired
 }
