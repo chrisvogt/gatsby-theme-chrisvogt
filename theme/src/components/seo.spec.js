c7ea953c3e1a6db6888e642ec066681c4bab8f99
Chris Vogt <mail@chrisvogt.me>
2020-07-09 02:18:27 -0700

    WIP: add unit tests

---
+++ theme/src/components/seo.spec.js
@@ -0,0 +1,44 @@
+import { mount } from 'enzyme'
+import { useThemeUI } from 'theme-ui'
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import SEO from './seo'
+import useSiteMetadata from '../hooks/use-site-metadata'
+
+jest.mock('../hooks/use-site-metadata')
+jest.mock('theme-ui')
+
+const mockSiteMetadata = {
+  languageCode: 'en',
+  title: 'My Personal Blog & Portfolio',
+  titleTemplate: '%s | My Test Site'
+}
+
+const mockBackgroundColor = '#aeaeae'
+const mockThemeUIResult = {
+  theme: {
+    colors: {
+      background: mockBackgroundColor
+    }
+  }
+}
+
+describe.only('SEO', () => {
+  useSiteMetadata.mockImplementation(() => mockSiteMetadata)
+  useThemeUI.mockImplementation(() => mockThemeUIResult)
+
+  describe('page title', () => {
+    it('defaults to the site title', () => {
+      const wrapper = mount(<SEO />)
+      expect(Helmet.peek().title).toBe(
+        'My Personal Blog & Portfolio | My Test Site'
+      )
+    })
+
+    it('accepts a custom page title', () => {
+      const wrapper = mount(<SEO title='derp' />)
+      expect(Helmet.peek().title).toBe('derp | My Test Site')
+    })
+  })
+})
