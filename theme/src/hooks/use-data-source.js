d61eb92a6e748f3c061686fc14675f82823d6956
Chris Vogt <mail@chrisvogt.me>
2020-08-19 08:53:31 -0700

    WIP: begin to add support for widget errors

--- theme/src/hooks/use-data-source.js
+++ theme/src/hooks/use-data-source.js
@@ -3,23 +3,28 @@ import { useEffect, useState } from 'react'
 
 const useDataSource = dataSourceURL => {
   const [data, setData] = useState([])
+  const [error, setError] = useState()
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     ;(async () => {
-      console.log(dataSourceURL)
-      const { data } = await axios.get(dataSourceURL)
-      console.log(dataSourceURL, data)
-      // NOTE(chrisvogt): factor out data.payload from here once all endpoints
-      // return data in a consistent structure.
-      setData(data.payload || data)
-      setIsLoading(false)
+      try {
+        const { data } = await axios.get(dataSourceURL)
+        // NOTE(chrisvogt): factor out data.payload from here once all endpoints
+        // return data in a consistent structure.
+        setData(data.payload || data)
+        setIsLoading(false)
+      } catch (err) {
+        console.error('Failed to fetch response from data source', err)
+        setError(err)
+      }
     })()
   }, [dataSourceURL])
 
   return {
-    isLoading,
-    data
+    data,
+    error,
+    isLoading
   }
 }
 
