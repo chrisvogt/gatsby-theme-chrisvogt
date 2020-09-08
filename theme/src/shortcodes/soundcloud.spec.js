ee6d4b6a5e9a9ceb08f45e41950e41fe556e1d1d
Chris Vogt <mail@chrisvogt.me>
2020-09-07 23:24:17 -0700

    bump version

---
+++ theme/src/shortcodes/soundcloud.spec.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import SoundCloud from './soundcloud'
+
+describe('SoundCloud Shortcode', () => {
+  it('matches the snapshot', () => {
+    const tree = renderer
+      .create(<SoundCloud soundcloudId='880888540' />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders a default title if one is not provided', () => {
+    const testRenderer = renderer.create(
+      <SoundCloud soundcloudId='880888540' />
+    )
+    const testInstance = testRenderer.root
+    expect(testInstance.findByType('iframe').props.title).toEqual(
+      'Song on SoundCloud'
+    )
+  })
+})
