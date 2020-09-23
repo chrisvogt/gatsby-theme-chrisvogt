6e2666758f4525715590ca6445a47a6347b9c2db
Chris Vogt <mail@chrisvogt.me>
2020-09-22 22:21:19 -0700

    design: polish 404 page styles

--- theme/src/pages/404.js
+++ theme/src/pages/404.js
@@ -1,12 +1,12 @@
 /** @jsx jsx */
-import { Container, Flex, jsx } from 'theme-ui'
-import { Fragment, useRef } from 'react'
+import { Box, Container, Flex, Grid, jsx } from 'theme-ui'
+import { useRef } from 'react'
 import Lottie from 'lottie-react-web'
 
 import Layout from '../components/layout'
 
 const options = Object.freeze({
-  animationData: require('../../assets/astronaout.json'),
+  animationData: require('../../assets/astronaut.json'),
   autoplay: true,
   loop: true
 })
@@ -15,25 +15,40 @@ const NotFoundPage = () => {
   const ref = useRef()
   return (
     <Layout>
-      <div></div>
-      <Container>
-        <div sx={{ textAlign: `center` }}>
-          <Lottie
-            key='$floatingAstronaut'
-            ref={ref}
-            height='250px'
-            options={options}
-          />
-          <h1>404</h1>
-          <p>
-            Lost in space? Why not go{' '}
-            <a title='Home page' href='/'>
-              home
-            </a>
-            .
-          </p>
-        </div>
-      </Container>
+      <Flex sx={{ flex: 1 }}>
+        <Container>
+          <Grid
+            gap={4}
+            sx={{ gridTemplateColumns: [`auto`, `auto`, `1fr 70%`] }}
+          >
+            <Box>
+              <Lottie
+                key='$floatingAstronaut'
+                ref={ref}
+                height='50vh'
+                options={options}
+              />
+            </Box>
+            <Box
+              sx={{
+                display: `flex`,
+                flexDirection: `column`,
+                justifyContent: `center`,
+                textAlign: [`center`, `center`, `left`]
+              }}
+            >
+              <h1 sx={{ my: 0 }}>404</h1>
+              <p>
+                Lost in space? Why not go{' '}
+                <a title='Home page' href='/'>
+                  home
+                </a>
+                .
+              </p>
+            </Box>
+          </Grid>
+        </Container>
+      </Flex>
     </Layout>
   )
 }
