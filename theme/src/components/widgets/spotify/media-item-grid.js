d4ec88d18e917030bed54dc12c2f2595b935332a
Chris Vogt <mail@chrisvogt.me>
2021-01-08 02:19:20 -0800

    WIP: improve the transition between focused/unfocused media states

--- theme/src/components/widgets/spotify/media-item-grid.js
+++ theme/src/components/widgets/spotify/media-item-grid.js
@@ -2,6 +2,7 @@
 import { jsx, Styled } from 'theme-ui'
 import Placeholder from 'react-placeholder'
 import { RectShape } from 'react-placeholder/lib/placeholders'
+import { useState } from 'react'
 
 import { floatOnHover } from '../../../gatsby-plugin-theme-ui/abstracts/shadows'
 import Tooltip from '../../tooltip'
@@ -23,43 +24,56 @@ const placeholders = Array(12)
     </div>
   ))
 
-const MediaItemGrid = ({ isLoading, items = [] }) => (
-  <div
-    sx={{
-      display: `grid`,
-      gridGap: [3, 2, 2, 3],
-      gridTemplateColumns: [`repeat(4, 1fr)`, `repeat(6, 1fr)`]
-    }}
-  >
-    <Placeholder ready={!isLoading} customPlaceholder={placeholders}>
-      {items.map(({
-          id,
-          spotifyURL,
-          thumbnailURL,
-          tooltipContent
-        }) => (
-          <Tooltip placement='top' trigger={['hover', 'focus']} tooltip={tooltipContent}>
-            <Styled.a
-              href={spotifyURL}
-              key={id}
-            >
-              <img
-                alt='cover artwork'
-                crossOrigin='anonymous'
-                src={thumbnailURL}
-                sx={{
-                  ...floatOnHover,
-                  boxShadow: `md`,
-                  borderRadius: `4px`,
-                  objectFit: 'cover',
-                  width: '100%'
-                }}
-              />
-            </Styled.a>
-          </Tooltip>
-        ))}
-    </Placeholder>
-  </div>
-)
+const MediaItemGrid = ({ isLoading, items = [] }) => {
+  const [currentMediaId, setCurrentMediaId] = useState(false)
+  return (
+    <div
+      className={`media-item_grid ${!!currentMediaId ? 'media-item_grid--interacting' : null}`}
+      sx={{
+        display: `grid`,
+        gridGap: [3, 2, 2, 3],
+        gridTemplateColumns: [`repeat(4, 1fr)`, `repeat(6, 1fr)`]
+      }}
+    >
+      <Placeholder ready={!isLoading} customPlaceholder={placeholders}>
+        {items.map(({
+            id,
+            spotifyURL,
+            thumbnailURL,
+            tooltipContent
+          }) => {
+            return (
+              <Tooltip
+                key={id}
+                placement='top'
+                tooltip={tooltipContent}
+                trigger={['hover', 'focus']}
+              >
+                <Styled.a
+                  className={`media-item_media${currentMediaId === id ? ' media-item--focused' : ''}`}
+                  href={spotifyURL}
+                  onMouseEnter={() => setCurrentMediaId(id)}
+                  onMouseLeave={() => setCurrentMediaId(false)}
+                >
+                  <img
+                    alt='cover artwork'
+                    crossOrigin='anonymous'
+                    src={thumbnailURL}
+                    sx={{
+                      ...floatOnHover,
+                      boxShadow: `md`,
+                      borderRadius: `4px`,
+                      objectFit: 'cover',
+                      width: '100%'
+                    }}
+                  />
+                </Styled.a>
+              </Tooltip>
+            )
+          })}
+      </Placeholder>
+    </div>
+  )
+}
 
 export default MediaItemGrid
