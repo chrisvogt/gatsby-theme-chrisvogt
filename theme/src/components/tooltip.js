308bca76168b2f8ef04d0df636c9d614336be96f
Chris Vogt <mail@chrisvogt.me>
2021-01-09 00:44:15 -0800

    fix: tooltip text color in dark mode

--- theme/src/components/tooltip.js
+++ theme/src/components/tooltip.js
@@ -1,47 +1,57 @@
-import React from 'react';
+/** @jsx jsx */
+import { Heading, jsx, useThemeUI, Link } from 'theme-ui'
 import TooltipTrigger from 'react-popper-tooltip';
+import getIsDarkMode from '../helpers/isDarkMode'
 import 'react-popper-tooltip/dist/styles.css';
 
-const Tooltip = ({children, tooltip, hideArrow, ...props}) => (
-  <TooltipTrigger
-    {...props}
-    tooltip={({
-      arrowRef,
-      tooltipRef,
-      getArrowProps,
-      getTooltipProps,
-      placement
-    }) => (
-      <div
-        {...getTooltipProps({
-          ref: tooltipRef,
-          className: 'tooltip-container'
-        })}
-      >
-        {!hideArrow && (
-          <div
-            {...getArrowProps({
-              ref: arrowRef,
-              className: 'tooltip-arrow',
-              'data-placement': placement
-            })}
-          />
-        )}
-        {tooltip}
-      </div>
-    )}
-  >
-    {({getTriggerProps, triggerRef}) => (
-      <span
-        {...getTriggerProps({
-          ref: triggerRef,
-          className: 'trigger'
-        })}
-      >
-        {children}
-      </span>
-    )}
-  </TooltipTrigger>
-);
+const Tooltip = ({children, tooltip, hideArrow, ...props}) => {
+  const { colorMode } = useThemeUI()
+  const isDarkMode = getIsDarkMode(colorMode)
+  return (
+    <TooltipTrigger
+      {...props}
+      tooltip={({
+        arrowRef,
+        tooltipRef,
+        getArrowProps,
+        getTooltipProps,
+        placement
+      }) => (
+        <div
+          {...getTooltipProps({
+            ref: tooltipRef,
+            className: 'tooltip-container',
+          })}
+          sx={{
+            border: theme => isDarkMode ? `none` : theme.colors.gray[8],
+            ...(isDarkMode ? { color: `text` } : {})
+          }}
+        >
+          {!hideArrow && (
+            <div
+              {...getArrowProps({
+                ref: arrowRef,
+                className: 'tooltip-arrow',
+                'data-placement': placement
+              })}
+            />
+          )}
+          {tooltip}
+        </div>
+      )}
+    >
+      {({getTriggerProps, triggerRef}) => (
+        <span
+          {...getTriggerProps({
+            ref: triggerRef,
+            className: 'trigger'
+          })}
+        >
+          {children}
+        </span>
+      )}
+    </TooltipTrigger>
+  )
+}
 
-export default Tooltip;
+export default Tooltip
