54523a398e8bffdd088ed1e998bb012653ea7410
Chris Vogt <mail@chrisvogt.me>
2021-01-30 01:15:10 -0800

    WIP: migrate Goodreads

--- theme/src/components/widgets/goodreads/goodreads-widget.js
+++ theme/src/components/widgets/goodreads/goodreads-widget.js
@@ -1,41 +1,56 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import get from 'lodash/get'
 
 import {
   getGoodreadsUsername,
   getGoodreadsWidgetDataSource
 } from '../../../selectors/metadata'
 
-import useDataSource from '../../../hooks/use-data-source'
+import fetchDataSource from '../../../actions/fetchDataSource'
+import selectMetricsPayload from '../../../selectors/selectMetricsPayload'
 import useSiteMetadata from '../../../hooks/use-site-metadata'
 
 import CallToAction from '../call-to-action'
-import RecentlyReadBooks from './recently-read-books'
 import ProfileMetricsBadge from '../profile-metrics-badge'
+import RecentlyReadBooks from './recently-read-books'
 import UserStatus from './user-status'
 import Widget from '../widget'
 import WidgetHeader from '../widget-header'
 
-const getStatusFromUpdates = updates =>
-  updates.length > 0
-    ? updates.find(({ type }) => type === 'userstatus' || type === 'review')
-    : {}
+const WIDGET_ID = 'goodreads'
 
-export default () => {
-  const metadata = useSiteMetadata()
+const getBooks = state => {
+  const booksCollection = get(
+    state,
+    'widgets.dataSources.goodreads.data.collections.recentlyReadBooks',
+    []
+  )
 
-  const goodreadsUsername = getGoodreadsUsername(metadata)
-  const dataSource = getGoodreadsWidgetDataSource(metadata)
+  if (!booksCollection.length) {
+    return {}
+  }
 
-  const { isLoading, data } = useDataSource(dataSource)
+  const books = booksCollection
+    // NOTE(chrisvogt): only select books with thumbnails, since we render those
+    // into image elements
+    .filter(({ thumbnail }) => Boolean(thumbnail))
+    .slice(0, 12)
 
-  const {
-    collections: {
-      updates = [],
-      recentlyReadBooks = []
-    } = {},
-    profile: { friendsCount, name: profileName, readCount } = {}
-  } = data
+  return books
+}
+
+const getMetrics = state => {
+  const friendsCount = get(
+    state,
+    'widgets.dataSources.goodreads.data.profile.friendsCount'
+  )
+  const readCount = get(
+    state,
+    'widgets.dataSources.goodreads.data.profile.readCount'
+  )
 
   const metrics = [
     {
@@ -50,11 +65,67 @@ export default () => {
     }
   ]
 
-  const books =
-    recentlyReadBooks.length &&
-    recentlyReadBooks.filter(({ thumbnail }) => Boolean(thumbnail)).slice(0, 12)
+  return metrics
+}
 
-  const status = updates.length ? getStatusFromUpdates(updates) : {}
+const getUserStatus = state => {
+  const updates = get(
+    state,
+    'widgets.dataSources.goodreads.data.collections.updates',
+    []
+  )
+
+  if (!updates.length) {
+    return {}
+  }
+
+  const userStatus = updates.find(
+    ({ type }) => type === 'userstatus' || type === 'review'
+  )
+
+  return userStatus
+}
+
+export default () => {
+  const dispatch = useDispatch()
+  const metadata = useSiteMetadata()
+  const goodreadsUsername = getGoodreadsUsername(metadata)
+  const goodreadsDataSource = getGoodreadsWidgetDataSource(metadata)
+
+  useEffect(() => {
+    dispatch(
+      fetchDataSource(WIDGET_ID, goodreadsDataSource, selectMetricsPayload)
+    )
+  }, [dispatch, goodreadsDataSource])
+
+  const { books, isLoading, metrics, profileDisplayName, status } = useSelector(
+    state => ({
+      books: getBooks(state),
+      isLoading: get(state, 'widgets.dataSources.github.state') !== 'SUCCESS',
+      metrics: getMetrics(state),
+      profileDisplayName: get(
+        state,
+        'widgets.dataSources.goodreads.data.profile.name'
+      ),
+      status: getUserStatus(state)
+    })
+  )
+
+  // const { isLoading, data } = useDataSource(dataSource)
+
+  // const {
+  //   collections: {
+  //     updates = [],
+  //     recentlyReadBooks = []
+  //   } = {},
+  //   profile: { friendsCount, name: profileName, readCount } = {}
+  // } = data
+
+  // const books =
+  //   recentlyReadBooks.length &&
+  //   recentlyReadBooks.filter(({ thumbnail }) => Boolean(thumbnail)).slice(0, 12)
+
+  // const status = updates.length ? getStatusFromUpdates(updates) : {}
 
   const callToAction = (
     <CallToAction
@@ -76,7 +147,7 @@ export default () => {
       <RecentlyReadBooks isLoading={isLoading} books={books} />
 
       <UserStatus
-        actorName={profileName}
+        actorName={profileDisplayName}
         isLoading={isLoading}
         status={status}
       />
