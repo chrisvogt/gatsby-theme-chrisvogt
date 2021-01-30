35b7cf1ea4d5175806d397f2905718571f0f912f
Chris Vogt <mail@chrisvogt.me>
2021-01-30 00:42:05 -0800

    WIP: add GitHub widget

--- theme/src/actions/fetchDataSource.js
+++ theme/src/actions/fetchDataSource.js
@@ -3,7 +3,6 @@ import isFunction from 'lodash/isFunction'
 
 const fetchDataSource = (dataSourceId, dataSourceURL, selectorFunc) => {
   return async function fetchDataSourceThunk(dispatch) {
-    console.log(`Fetching data for ${dataSourceId}: ${dataSourceURL}`)
     try {
       const { data } = await axios.get(dataSourceURL)
       dispatch({
--- theme/src/components/widgets/github/github-widget.js
+++ theme/src/components/widgets/github/github-widget.js
@@ -1,6 +1,8 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import get from 'lodash/get'
 
 import CallToAction from '../call-to-action'
 import LastPullRequest from './last-pull-request'
@@ -9,59 +11,69 @@ import ProfileMetricsBadge from '../profile-metrics-badge'
 import Widget from '../widget'
 import WidgetHeader from '../widget-header'
 
+import fetchDataSource from '../../../actions/fetchDataSource'
 import {
   getGithubUsername,
   getGithubWidgetDataSource
 } from '../../../selectors/metadata'
-import selectPinnedItems from './selectors/get-pinned-items'
-import selectPullRequests from './selectors/get-pull-requests'
-import selectUser from './selectors/get-user'
+import selectMetricsPayload from '../../../selectors/selectMetricsPayload'
 
-import useDataSource from '../../../hooks/use-data-source'
 import useSiteMetadata from '../../../hooks/use-site-metadata'
 
+const getMetrics = state => {
+  const totalFollowersCount = get(
+    state,
+    'widgets.dataSources.github.data.user.followers.totalCount'
+  )
+  const totalFollowingCount = get(
+    state,
+    'widgets.dataSources.github.data.user.following.totalCount'
+  )
+
+  const metrics = [
+    {
+      displayName: 'Followers',
+      id: 'followers',
+      value: totalFollowersCount
+    },
+    {
+      displayName: 'Following',
+      id: 'following',
+      value: totalFollowingCount
+    }
+  ]
+
+  return metrics
+}
+
+const WIDGET_ID = 'github'
+
 const GitHubWidget = () => {
+  const dispatch = useDispatch()
   const metadata = useSiteMetadata()
   const githubUsername = getGithubUsername(metadata)
   const githubDataSource = getGithubWidgetDataSource(metadata)
 
-  const { isLoading, data } = useDataSource(githubDataSource)
-
-  const [latestPullRequest, setLatestPullRequests] = useState({})
-  const [metrics, setMetrics] = useState([])
-  const [pinnedItems, setPinnedItems] = useState([])
-
   useEffect(() => {
-    if (isLoading || Object.keys(data).length === 0) {
-      return
-    }
+    dispatch(fetchDataSource(WIDGET_ID, githubDataSource, selectMetricsPayload))
+  }, [dispatch, githubDataSource])
 
-    const pinnedItems = selectPinnedItems(data)
-    const pullRequest = selectPullRequests(data)
-
-    const {
-      followers: { totalCount: totalFollowersCount = 0 } = {},
-      following: { totalCount: totalFollowingCount = 0 } = {}
-    } = selectUser(data) || {}
-
-    const metrics = [
-      {
-        displayName: 'Followers',
-        id: 'followers',
-        value: totalFollowersCount
-      },
-      {
-        displayName: 'Following',
-        id: 'following',
-        value: totalFollowingCount
-      }
-    ]
-
-    setLatestPullRequests(pullRequest)
-    setPinnedItems(pinnedItems)
-
-    setMetrics(metrics)
-  }, [isLoading, data])
+  const { isLoading, lastPullRequest, metrics, pinnedItems } = useSelector(
+    state => ({
+      isLoading: get(state, 'widgets.dataSources.github.state') !== 'SUCCESS',
+      lastPullRequest: get(
+        state,
+        'widgets.dataSources.github.data.user.pullRequests.nodes[0]',
+        {}
+      ),
+      metrics: getMetrics(state),
+      pinnedItems: get(
+        state,
+        'widgets.dataSources.github.data.user.pinnedItems.nodes',
+        []
+      )
+    })
+  )
 
   const callToAction = (
     <CallToAction
@@ -78,13 +90,14 @@ const GitHubWidget = () => {
     <Widget id='github'>
       <WidgetHeader aside={callToAction}>GitHub</WidgetHeader>
       <ProfileMetricsBadge metrics={metrics} />
-      <PinnedItems items={pinnedItems} />
-      <LastPullRequest isLoading={isLoading} pullRequest={latestPullRequest} />
+      <PinnedItems
+        isLoading={isLoading}
+        items={pinnedItems}
+        placeholderCount={2}
+      />
+      <LastPullRequest isLoading={isLoading} pullRequest={lastPullRequest} />
     </Widget>
   )
 }
 
-/**
- * The container component for the GitHub widget.
- */
 export default GitHubWidget
