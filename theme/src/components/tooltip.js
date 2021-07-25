18d9c29953c5dc723d65a7db2d5b612cf22e76ed
Chris Vogt <mail@chrisvogt.me>
2021-07-25 10:37:42 -0700

    Remove unused code

--- theme/src/components/tooltip.js
+++ theme/src/components/tooltip.js
@@ -5,30 +5,6 @@ import { usePopperTooltip } from 'react-popper-tooltip';
 import getIsDarkMode from '../helpers/isDarkMode'
 import 'react-popper-tooltip/dist/styles.css'
 
-const theTooltip = () => (
-  <div
-    {...getTooltipProps({
-      ref: tooltipRef,
-      className: 'tooltip-container'
-    })}
-    sx={{
-      border: theme => (isDarkMode ? `none` : theme.colors.gray[8]),
-      ...(isDarkMode ? { color: `text` } : {})
-    }}
-  >
-    {!hideArrow && (
-      <div
-        {...getArrowProps({
-          ref: arrowRef,
-          className: 'tooltip-arrow',
-          'data-placement': placement
-        })}
-      />
-    )}
-    {tooltip}
-  </div>
-)
-
 const Tooltip = ({ children, tooltip, hideArrow, ...props }) => {
   const { colorMode } = useThemeUI()
   const isDarkMode = getIsDarkMode(colorMode)
