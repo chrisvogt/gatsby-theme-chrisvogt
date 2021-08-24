f8503dd0bcdbf675321567dacc40466fa343d3ac
Chris Vogt <mail@chrisvogt.me>
2021-08-23 21:40:28 -0700

    fix: syntax highlighting in GatsbyJS posts using PrismJS (#116)

--- theme/src/gatsby-plugin-theme-ui/styles.js
+++ theme/src/gatsby-plugin-theme-ui/styles.js
@@ -207,17 +207,5 @@ export default {
 
   '.text-center': {
     textAlign: `center`
-  },
-
-  '.gatsby-highlight pre[class*="language-"]': {
-    padding: 0,
-    overflow: `initial`,
-    float: `left`,
-    minWidth: `100%`
-  },
-
-  /* Adjust the position of the line numbers */
-  '.gatsby-highlight pre[class*="language-"].line-numbers': {
-    paddingLeft: `2.8em`
   }
 }
