a95705cdd1fe446ed81efbd493c99c29f3b4722b
Chris Vogt <mail@chrisvogt.me>
2022-07-31 08:41:38 -0700

    Work on consolidating theme into one file

--- theme/src/gatsby-plugin-theme-ui/abstracts/fonts.js
+++
@@ -1,5 +0,0 @@
-export default {
-  sans: '-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif',
-  serif: 'Iowan Old Style, Apple Garamond, Baskerville, Times New Roman, Droid Serif, Times, Source Serif Pro, serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
-  mono: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace'
-}
--- theme/src/gatsby-plugin-theme-ui/index.js
+++ theme/src/gatsby-plugin-theme-ui/index.js
@@ -1,13 +1,25 @@
-import colors from './abstracts/colors'
-import fonts from './abstracts/fonts'
-
-import { card, metricCard, PostCard } from './components/card'
-
 import { floatOnHover } from './abstracts/shadows'
-
+import { merge } from 'theme-ui'
 import styles, { themePreset } from './styles'
 
-import { merge } from 'theme-ui'
+const card = {
+  backgroundColor: `var(--theme-ui-colors-panel-background)`,
+  color: 'var(--theme-ui-colors-panel-text)',
+  borderRadius: `3px`,
+  boxShadow: `default`,
+  flexGrow: 1,
+  padding: 3,
+  textDecoration: `none`
+};
+
+const fonts = {
+  sans: '-apple-system, BlinkMacSystemFont, avenir next, avenir, helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif',
+  serif: 'Iowan Old Style, Apple Garamond, Baskerville, Times New Roman, Droid Serif, Times, Source Serif Pro, serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol',
+  mono: 'Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace'
+};
+
+// const cardBackgroundDark = '#252e3c'
+// const pageBackgroundDark = '#2d3748'
 
 export default merge(themePreset, {
   useColorSchemeMediaQuery: true,
@@ -34,12 +46,22 @@ export default merge(themePreset, {
       ...card,
       ...floatOnHover,
       borderBottom: `none`, // from actionCardDark
-      borderLeft: theme => `3px solid ${theme.colors.primary}`
+      borderLeft: theme => `3px solid ${theme.colors.primary}`,
+      dark: {
+        backgroundColor: 'green'
+      }
     },
 
     metricCard: {
       ...card,
-      ...metricCard
+      backgroundColor: `var(--theme-ui-colors-panel-background)`,
+      boxShadow: `none`,
+      color: 'var(--theme-ui-colors-panel-text)',
+      span: {
+        fontFamily: `heading`,
+        fontWeight: `bold`,
+        padding: 2
+      }
     },
 
     /* The following styles represent specific card components, indicated in PascalCase. */
@@ -64,10 +86,52 @@ export default merge(themePreset, {
       backgroundColor: `#1e2530`
     },
 
-    PostCard
+    PostCard: {
+      ...card,
+      ...floatOnHover,
+      backgroundColor: `var(--theme-ui-colors-panel-background)`,
+      color: 'var(--theme-ui-colors-panel-text)',
+      display: `flex`,
+      height: `100%`,
+      flexDirection: `column`,
+      '.card-media': {
+        mb: 2,
+        height: `100%`,
+        overflow: `hidden`
+      },
+      '.read-more-icon': {
+        display: `inline`,
+        transition: `all 250ms ease-in`,
+        opacity: 0,
+        paddingLeft: 0
+      },
+      '&:hover .read-more-icon': {
+        opacity: 1,
+        paddingLeft: `8px`
+      }
+    }
   },
 
-  colors,
+  colors: {
+    accent: `deeppink`,
+    background: `#fcfcfc`,
+    'panel-background': `white`,
+    'panel-divider': theme => `1px solid ${theme.colors.gray[3]}`,
+    'panel-highlight': theme => theme.colors.gray[1],
+    modes: {
+      dark: {
+        background: `#2d3748`,
+        'panel-background': `#252e3c`,
+        'panel-divider': theme => `1px solid ${theme.colors.gray[8]}`,
+        'panel-highlight': theme => theme.colors.gray[8],
+        text: `white`
+      }
+    },
+    primary: `#1E90FF`,
+    secondary: `#711E9B`,
+    secondaryGradient: `linear-gradient(45deg, #4527a0 0%, #711e9b 100%)`,
+    text: `#2d3748`
+  },
 
   fonts: {
     body: fonts.serif,
