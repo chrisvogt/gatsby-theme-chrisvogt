d22843f28f085b5d82476969bbc54f1d511f7bbb
Chris Vogt <mail@chrisvogt.me>
2022-08-12 03:57:51 -0700

    Begin to fill in about site work

--- theme/src/pages/about/site.js
+++ theme/src/pages/about/site.js
@@ -25,8 +25,14 @@ const AboutSitePage = () => (
           </Themed.h1>
 
           <p>
-            This website first came into existence September 2012 as a timeline of my social feeds. It spent the next decade evolving into the hybrid blog and social dashboard it is today. This site [has been crawled over 100 times](https://web.archive.org/web/20120801000000*/https://www.chrisvogt.me) by the Wayback Machine.
+            This website first came into existence September 2012 as a timeline of my social feeds. It spent the next decade evolving into the hybrid blog and social dashboard it is today. This site <a title='Wayback Machine for www.chrisvogt.me' href='https://web.archive.org/web/20120801000000*/https://www.chrisvogt.me'>has been crawled over 100 times</a> by the Wayback Machine.
           </p>
+
+          <Themed.h2>Setup</Themed.h2>
+
+          <Themed.h3>Data flow diagram</Themed.h3>
+
+          <iframe width="100%" height="432" src="https://miro.com/app/live-embed/uXjVPfHnU9E=/?moveToViewport=-683,-894,1446,1337&embedId=597705594823" frameborder="0" scrolling="no" allowfullscreen></iframe>
 
           <p>Lorum ipsum...</p>
         </Container>
