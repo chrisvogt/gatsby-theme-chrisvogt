8ac375c3c37a1bc0e88361f7b5b80a813ff46898
Chris Vogt <mail@chrisvogt.me>
2022-08-14 02:06:35 -0700

    WIP: refactor to tests and connect constant

--- theme/src/actions/fetchDataSource.js
+++ theme/src/actions/fetchDataSource.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import { FETCH_DATASOURCE_SUCCESS, FETCH_DATASOURCE_FAILURE } from '../reducers/widgets'
 
 const fetchDataSource = (widgetId, url) => {
   return async function fetchDataSourceThunk(dispatch) {
     try {
       const { data } = await axios.get(url)
       dispatch({
-        type: 'FETCH_DATASOURCE_SUCCESS',
+        type: FETCH_DATASOURCE_SUCCESS,
         payload: {
           widgetId,
           data
@@ -13,7 +14,7 @@ const fetchDataSource = (widgetId, url) => {
       })
     } catch (error) {
       dispatch({
-        type: 'FETCH_DATASOURCE_FAILURE',
+        type: FETCH_DATASOURCE_FAILURE,
         payload: {
           widgetId,
           error
