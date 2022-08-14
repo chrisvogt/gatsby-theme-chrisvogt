e3cb389940a4e45464120e33b4943a9d6e2847d7
Chris Vogt <mail@chrisvogt.me>
2022-08-14 08:55:00 -0700

    WIP: improve code coverage

---
+++ theme/src/hooks/use-site-metadata.spec.js
@@ -0,0 +1,32 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useStaticQuery } from 'gatsby'
+import useSiteMetadata from './use-site-metadata'
+
+const data = {
+  site: {
+    siteMetadata: {
+      baseURL: 'https://www.fake-site.com',
+      description: 'A fake website for unit tests!',
+      footerText: '© 2022 Chris Vogt',
+      headline: 'My New Website',
+      subhead: 'A place for my stuff on the web'
+    }
+  }
+}
+
+jest.mock('gatsby')
+
+describe('useSiteMetadata', () => {
+  beforeEach(() => {
+    useStaticQuery.mockImplementation(() => data)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('returns the site metadata', () => {
+    const { result } = renderHook(() => useSiteMetadata())
+    expect(result.current).toEqual(data.site.siteMetadata)
+  })
+})
