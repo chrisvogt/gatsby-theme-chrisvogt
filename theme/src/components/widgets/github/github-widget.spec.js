494e032f7d7886f70d9c71f32037dd8853978a4a
Chris Vogt <mail@chrisvogt.me>
2022-08-14 23:10:55 -0700

    WIP: add a loading state test for each widget

---
+++ theme/src/components/widgets/github/github-widget.spec.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import GitHubWidget from './github-widget'
+import renderer from 'react-test-renderer'
+import { TestProviderWithState } from '../../../testUtils'
+import useSiteMetadata from '../../../hooks/use-site-metadata'
+
+jest.mock('../../../hooks/use-site-metadata')
+
+const mockSiteMetadata = {
+  widgets: {
+    github: {
+      username: 'mockusername',
+      widgetDataSource: 'https://fake-api.chrisvogt.me/social/github'
+    }
+  }
+}
+
+describe('GitHub Widget', () => {
+  beforeEach(() => {
+    useSiteMetadata.mockImplementation(() => mockSiteMetadata)
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('matches the loading state snapshot', () => {
+    const tree = renderer
+      .create(
+        <TestProviderWithState>
+          <GitHubWidget />
+        </TestProviderWithState>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
