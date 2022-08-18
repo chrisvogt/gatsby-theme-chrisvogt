01bdd60c577d5f484385e23da9ca7ab06376dcd4
Chris Vogt <mail@chrisvogt.me>
2022-08-17 22:12:20 -0700

    WIP: remove unfinished content

--- theme/src/components/home-header-content.js
+++ theme/src/components/home-header-content.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Container, Flex, Link, Themed } from 'theme-ui'
+import { jsx, Flex, Themed } from 'theme-ui'
 
 /**
  * Home Page Header Content
@@ -10,7 +10,7 @@ import { jsx, Container, Flex, Link, Themed } from 'theme-ui'
 const HomeHeaderContent = ({ headline }) => (
   <Flex sx={{ width: '100%', justifyContent: 'center' }}>
     <div sx={{
-      'max-width': ['90%', '75%'],
+      maxWidth: ['90%', '75%'],
       pt: 2,
       pb: 4
     }}>
@@ -23,14 +23,6 @@ const HomeHeaderContent = ({ headline }) => (
         website is a digital garden where I share things I'm working
         on or excited about.
       </p>
-      <Flex sx={{ fontSize: 2, gap: `2em` }}>
-        <Link href='/about/me'>
-          More about me &rarr;
-        </Link>
-        <Link href='/about/site'>
-          More about this site &rarr;
-        </Link>
-      </Flex>
     </div>
   </Flex>
 )
