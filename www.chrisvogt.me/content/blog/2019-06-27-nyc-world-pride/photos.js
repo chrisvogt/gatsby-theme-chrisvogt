0245e0f49e41e6855e322f3391741bd45ff4f794
Chris Vogt <mail@chrisvogt.me>
2023-08-20 16:57:51 -0700

    feat: use next-gen image formats for all Cloudinary images (#168)
    This PR updates all images hosted on Cloudinary to use the **f_auto**
    feature, allowing browsers that support them to use next-gen image
    formats.
    
    https://cloudinary.com/blog/transparent_webp_format_cdn_delivery_based_on_visitors_browsers

--- www.chrisvogt.me/content/blog/2019-06-27-nyc-world-pride/photos.js
+++ www.chrisvogt.me/content/blog/2019-06-27-nyc-world-pride/photos.js
@@ -1,186 +1,186 @@
 export const photos = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/5EA20752-A9D5-49F4-A812-8E36A11341AF.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/5EA20752-A9D5-49F4-A812-8E36A11341AF',
     width: 1,
     height: 1
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6622.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6622',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6623.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6623',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6625.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6625',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6628.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6628',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6631.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6631',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6642.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6642',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6644.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6644',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6651.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6651',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6658.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6658',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6715.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6715',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6719.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6719',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6724.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6724',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6736.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6736',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6769.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6769',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6772.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6772',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_6990.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_6990',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7018.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7018',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7031.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7031',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7034.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7034',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7135.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7135',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7193.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7193',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7248.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7248',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7255.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7255',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7258.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7258',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7264.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7264',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7267.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7267',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7275.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7275',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7279.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7279',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7281.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7281',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7284.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7284',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7302.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7302',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7307.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7307',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7308.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7308',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7315.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7315',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7325.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7325',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1573540598/photos/2019-nyc-world-pride/IMG_7333.jpg',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1573540598/photos/2019-nyc-world-pride/IMG_7333',
     width: 3,
     height: 4
   }
