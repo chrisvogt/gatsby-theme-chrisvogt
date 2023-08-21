296460b67c1ac22c2609de364d938e66d0f087ed
Chris Vogt <mail@chrisvogt.me>
2023-08-20 17:36:28 -0700

    feat: upgrade deprecated dark mode toggle dependency

--- theme/src/components/color-toggle.js
+++ theme/src/components/color-toggle.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { DarkModeToggle } from 'react-dark-mode-toggle-2'
 import { useColorMode } from 'theme-ui'
-import DarkModeToggle from 'react-dark-mode-toggle'
+import isDarkMode from '../helpers/isDarkMode'
 
 export default () => {
   const [colorMode, setColorMode] = useColorMode()
+
   return (
     <DarkModeToggle
       onChange={() => setColorMode(colorMode === 'default' ? 'dark' : 'default')}
+      isDarkMode={isDarkMode(colorMode)}
       checked={colorMode === 'dark'}
       size={80}
     />
