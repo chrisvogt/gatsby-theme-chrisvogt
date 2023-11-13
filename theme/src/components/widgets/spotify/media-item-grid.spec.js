e638578d490405539bc1ae36bbdfd582e0a69a79
Chris Vogt <mail@chrisvogt.me>
2023-11-12 19:16:07 -0800

    SQUASH: add snapshots for the media item grid

---
+++ theme/src/components/widgets/spotify/media-item-grid.spec.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import MediaItemGrid from './media-item-grid'
+
+const mockItems = [
+  {
+    id: 'ITEM-1',
+    details: 'Item #1',
+    spotifyURL: 'https://www.google.com/',
+    thumbnailURL: 'http://placekitten.com/200/200'
+  }
+]
+
+describe('MediaItemGrid', () => {
+  it('matches loading state snapshot', () => {
+    const tree = renderer.create(<MediaItemGrid isLoading={false} items={mockItems} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('matches the ready state snapshot state', () => {
+    const tree = renderer.create(<MediaItemGrid isLoading={true} items={[]} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('updates the ', () => {
+    const tree = renderer.create(<MediaItemGrid isLoading={true} items={[]} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
