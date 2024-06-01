3a3be8b1e4cb3ad05a117a0b53629d58743639a4
Chris Vogt <mail@chrisvogt.me>
2024-06-01 12:33:11 -0700

    SQUASHME: reinstall Google Font using Gatsby plugin

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -1,9 +1,6 @@
 import 'prismjs/themes/prism-solarizedlight.css'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
-import '@fontsource/crimson-text'; // Defaults to weight 400
-import '@fontsource/crimson-text/700.css'; // Weight 700 (Bold)
-
 import './src/styles/global.css'
 
 export { default as wrapRootElement } from './wrapRootElement'
