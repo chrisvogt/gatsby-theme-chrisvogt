25ba8a865aa3f1420b82d277e5ad77d88ce2b80c
Chris Vogt <mail@chrisvogt.me>
2024-06-02 23:18:05 -0700

    feat: highlight current section in home nav (#178)
    This PR updates the home navigation widget to recognize and signal the
    current section being interacted with.
    
    
    https://github.com/chrisvogt/gatsby-theme-chrisvogt/assets/1934719/bbbdcd9c-c8e6-4b86-8b22-c1ada22a7ccb

--- theme/src/templates/media.js
+++ theme/src/templates/media.js
@@ -46,7 +46,7 @@ const MediaTemplate = ({ data: { mdx }, children }) => {
           py: 3
         }}
       >
-        <Container sx={{ height: `100%` }}>
+        <Container sx={{ height: `100%`, maxWidth: `800px` }}>
           {category && (
             <Themed.div sx={{ fontSize: [3, 4], variant: `text.title` }}>
               {category}
