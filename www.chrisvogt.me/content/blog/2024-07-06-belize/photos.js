8c8f8d876469a725f36f796dad85f1fac7251a31
Chris Vogt <mail@chrisvogt.me>
2024-07-07 13:46:52 -0700

    SQUASHME: add titles to images

--- www.chrisvogt.me/content/blog/2024-07-06-belize/photos.js
+++ www.chrisvogt.me/content/blog/2024-07-06-belize/photos.js
@@ -1,25 +1,30 @@
 export const gettingToBelize = [
   {
+    title: 'A fleet of Mayan Island Air Belize Cessna 182 Skylanes',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327677/chrisvogt-me/galleries/belize/20230701-IMG_0939.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'The cockpit of a Cessna 182 Skylane',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327670/chrisvogt-me/galleries/belize/20230701-IMG_0946.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'My pilots landing the Cessna 182 Skylane',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327670/chrisvogt-me/galleries/belize/20230701-IMG_0972.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Looking down over the Belize islands',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327674/chrisvogt-me/galleries/belize/20230708-CJV08677.webp',
     width: 3,
     height: 2
   },
   {
+    title: 'Looking down at Belize from the air',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327672/chrisvogt-me/galleries/belize/20230708-CJV08680.webp',
     width: 3,
     height: 2
@@ -28,16 +33,19 @@ export const gettingToBelize = [
 
 export const drivingAroundBelize = [
   {
+    title: 'The golf cart I rented from F & S Golf Carts and drove all over Ambergris Caye',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327694/chrisvogt-me/galleries/belize/20230705-CJV08580.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'Navigating the streets of San Pedro in a 3-row golf cart',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336288/chrisvogt-me/galleries/belize/20230706-IMG_1516.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'The road between San Pedro and Pelican Reef Villas',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327678/chrisvogt-me/galleries/belize/20230701-IMG_0990.webp',
     width: 4,
     height: 3
@@ -46,6 +54,7 @@ export const drivingAroundBelize = [
 
 export const whereWeStayed = [
   {
+    title: 'The swimming pool and pier at Pelican Reef Villas',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327689/chrisvogt-me/galleries/belize/20230708-CJV08648.webp',
     width: 3,
     height: 2
@@ -56,16 +65,19 @@ export const whereWeStayed = [
   //     height: 4
   //   },
   {
+    title: 'A hammock on the beach at Pelican Reef Villas, with the pier in the background',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327687/chrisvogt-me/galleries/belize/20230708-CJV08652.webp',
     width: 3,
     height: 2
   },
   {
+    title: 'A staff member at Pelican Reef Villas took our group photo, but accidentally focused on him instead of us',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327671/chrisvogt-me/galleries/belize/20230708-CJV08660.webp',
     width: 3,
     height: 2
   },
   {
+    title: 'Looking at the villas from the pier at Pelican Reef Villas',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327699/chrisvogt-me/galleries/belize/20230705-IMG_1448.webp',
     width: 4,
     height: 3
@@ -74,57 +86,68 @@ export const whereWeStayed = [
 
 export const exploringSanPedro = [
   {
+    title: 'The entrance to Caramba Restaurant, with fresh-caught red snapper on display. Pick the one you want them to cook.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327679/chrisvogt-me/galleries/belize/20230701-IMG_0992.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Walking through the San Pedro Island Artisan Market',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336288/chrisvogt-me/galleries/belize/20230707-IMG_1559.webp',
     width: 4,
     height: 3
   },
   {
     // Should I keep this? – it's a spice product photo
+    title: 'Spices from Caribbean Spice Belize. I have used up all of these since traveling and need to go back and get more.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336290/chrisvogt-me/galleries/belize/20230707-IMG_1558.webp',
     width: 3,
     height: 4
   },
   {
+    title: "Amaya's Mini Shop, a convenience store in San Pedro",
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720343410/chrisvogt-me/galleries/belize/20230703-IMG_1076.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Looking down the street in San Pedro, Belize. Golf carts park along the side of the road.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327682/chrisvogt-me/galleries/belize/20230703-IMG_1077.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'The Black Pearl Restaurant and Bar in San Pedro, Belize',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327697/chrisvogt-me/galleries/belize/20230703-IMG_1065.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'A boat at a park in San Pedro, Belize',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327691/chrisvogt-me/galleries/belize/20230703-IMG_1082.webp',
     width: 4,
     height: 3
   },
   {
+    title: "A sign near Ramon's Village Resort in San Pedro, Belize",
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327690/chrisvogt-me/galleries/belize/20230702-IMG_1022.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'A crocodile warning sign near a lagoon in San Pedro, Belize',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327676/chrisvogt-me/galleries/belize/20230702-IMG_1018.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Swings at Island Wonder Belize in San Pedro',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336289/chrisvogt-me/galleries/belize/20230706-IMG_1527.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Lounge chairs, swings, and palm trees line the beach at Island Wonder Belize in San Pedro',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336289/chrisvogt-me/galleries/belize/20230706-IMG_1543.webp',
     width: 4,
     height: 3
@@ -138,26 +161,31 @@ export const exploringSanPedro = [
 
 export const secretBeach = [
   {
+    title: 'Nick riding in the passenger seat while his cousin drives us to Secret Beach in a golf cart',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327683/chrisvogt-me/galleries/belize/20230705-IMG_1401.webp',
     width: 4,
     height: 3
   },
   {
+    title: "The Secret Beach sign in San Pedro, Belize. My skin is peeling from sunburn, but I'm still smiling.",
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327692/chrisvogt-me/galleries/belize/20230705-IMG_1408.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'Looking out past Secret Beach in Belize',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327676/chrisvogt-me/galleries/belize/20230705-CJV08591.webp',
     width: 3,
     height: 2
   },
   {
+    title: 'Rose Spa at Secret Beach in Belize. The spa is on a pier over the water.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327679/chrisvogt-me/galleries/belize/20230705-CJV08589.webp',
     width: 3,
     height: 2
   },
   {
+    title: 'A view of the water near Rose Spa at Secret Beach, and people snorkeling in the distance.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327675/chrisvogt-me/galleries/belize/20230705-CJV08587.webp',
     width: 3,
     height: 2
@@ -166,50 +194,60 @@ export const secretBeach = [
 
 export const cayeCaulker = [
   {
+    title: 'Boarding the San Pedro Belize Express Water Taxi to Caye Caulker.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336287/chrisvogt-me/galleries/belize/20230707-IMG_1561.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'Inside of the ferry that shuttles people between San Pedro and Caye Caulker.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336289/chrisvogt-me/galleries/belize/20230707-IMG_1564.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'A shot of a pier and a boat at Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336287/chrisvogt-me/galleries/belize/20230707-IMG_1601.webp',
     width: 4,
     height: 3
   }, {
+    title: 'A boat docked at Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720344456/chrisvogt-me/galleries/belize/20230707-IMG_1598.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'A row of boats docked at Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336287/chrisvogt-me/galleries/belize/20230707-IMG_1614.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'A stingray swimming near the dock at Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327686/chrisvogt-me/galleries/belize/20230704-IMG_1251.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Me standing next to the Caye Caulker sign in Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327698/chrisvogt-me/galleries/belize/20230704-IMG_1269.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'The Split and signs for the Lazy Lizard Bar and Grill in Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327682/chrisvogt-me/galleries/belize/20230704-IMG_1275.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'Cafe by the Sea in Caye Caulker, Belize and Happy Lobster Restaurant.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327696/chrisvogt-me/galleries/belize/20230704-IMG_1262.webp',
     width: 4,
     height: 3
   },
   {
+    title: "People swimming and lounging at the Sip n' Dip in Caye Caulker, Belize. Where you sit in chairs on the water and drink cocktails.",
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327685/chrisvogt-me/galleries/belize/20230707-IMG_1590.webp',
     width: 4,
     height: 3
@@ -218,26 +256,31 @@ export const cayeCaulker = [
 
 export const cayeCaulkerFood = [
   {
+    title: 'It was lobster fest and there were Loads of Lobster. A menu of lobster-themed plates.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336288/chrisvogt-me/galleries/belize/20230707-IMG_1595.webp',
     width: 3,
     height: 4
   },
   {
+    title: "Lobster and chicken on the grill at Betty's Go Slow Grill. In island time, the food is ready when it's perfect.",
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327700/chrisvogt-me/galleries/belize/20230707-IMG_1574.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Lobster on the grill at Betty’s Go Slow Grill in Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336288/chrisvogt-me/galleries/belize/20230707-IMG_1572.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Nick got the chicken and plaintain plate at Betty’s Go Slow Grill in Caye Caulker, Belize.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327688/chrisvogt-me/galleries/belize/20230707-IMG_1577.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Lobsterfest',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327697/chrisvogt-me/galleries/belize/20230707-IMG_1582.webp',
     width: 4,
     height: 3
@@ -246,26 +289,31 @@ export const cayeCaulkerFood = [
 
 export const swimmingWithSharks = [
   {
+    title: 'Nick smirking as we ride out to swim with sharks in Shark Ray Alley.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327673/chrisvogt-me/galleries/belize/20230704-IMG_1142.webp',
     width: 4,
     height: 3
   },
   {
+    title: 'Me getting splashed with water in my face (and eyes) as we ride out to swim with sharks. Which secretly terrifies me.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327674/chrisvogt-me/galleries/belize/20230704-IMG_1154.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'Our captain navigating the seas, searching for a manatee to show us.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327672/chrisvogt-me/galleries/belize/20230704-IMG_1183.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'The Hol Chan Marine Reserve Marine Station.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336289/chrisvogt-me/galleries/belize/20230706-IMG_1488.webp',
     width: 3,
     height: 4
   },
   {
+    title: 'A group of snorkelers prepare to jump into the water with sharks and stingrays at Hol Chan Marine Reserve.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720336287/chrisvogt-me/galleries/belize/20230706-IMG_1508.webp',
     width: 16,
     height: 9
@@ -274,11 +322,13 @@ export const swimmingWithSharks = [
 
 export const iguanas = [
   {
+    title: 'An iguana chilling on a structure near the Pelican Reef Villas.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327671/chrisvogt-me/galleries/belize/20230705-CJV08583.webp',
     width: 3,
     height: 2
   },
   {
+    title: 'Another iguana sunning itself on the rocks near Secret Beach.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327678/chrisvogt-me/galleries/belize/20230705-CJV08602.webp',
     width: 3,
     height: 2
@@ -286,6 +336,7 @@ export const iguanas = [
 ]
 
 export const flightHome = [{
+    title: 'Boarding a Cessna 208B Grand Caravan in San Pedro to hop over to Belize City for our flight home.',
     src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1720327680/chrisvogt-me/galleries/belize/20230708-IMG_1619.webp',
     width: 3,
     height: 4
