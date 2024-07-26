79ed3f29f439b1a6186f05f2014ccab35009c22d
Chris Vogt <mail@chrisvogt.me>
2024-07-25 22:56:21 -0700

    SQUASHME: fixup wrapRootElement

--- theme/wrapRootElement.js
+++ theme/wrapRootElement.js
@@ -1,15 +1,14 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui'
 import { Global } from '@emotion/react'
-import { MDXProvider } from '@mdx-js/react'
-import { Provider as ReduxProvider } from 'react-redux'
-import { ThemeUIProvider } from 'theme-ui'
+import { jsx } from 'theme-ui'
 import { MDXProvider, useMDXComponents } from '@mdx-js/react'
+import { Provider as ReduxProvider } from 'react-redux'
 import { Themed, useThemedStylesWithMdx } from '@theme-ui/mdx'
+import { ThemeUIProvider } from 'theme-ui'
 
+import Emoji from './src/shortcodes/emoji'
 import store from './src/store'
 import theme from './src/gatsby-plugin-theme-ui'
-import Emoji from './src/shortcodes/emoji'
 import YouTube from './src/shortcodes/youtube'
 
 const components = {
@@ -32,10 +31,6 @@ const WrapRootElement = ({ element }) => {
       </ThemeUIProvider>
     </ReduxProvider>
   )
-}
-
-WrapRootElement.propTypes = {
-  element: PropTypes.node.isRequired,
 }
 
 export const wrapRootElement = ({ element }) => (
