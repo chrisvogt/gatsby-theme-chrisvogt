7ee29509309ed5a95e67c88a01dd17b32a136fd1
Chris Vogt <mail@chrisvogt.me>
2024-07-25 22:34:13 -0700

    content: add new blog post breaking down hosting costs

---
+++ www.chrisvogt.me/gatsby-node.js
@@ -0,0 +1,10 @@
+exports.createPages = async ({ graphql, actions }) => {
+  const { createRedirect } = actions
+
+  createRedirect({ // Originally published to the wrong category
+    fromPath: '/personal/2024-my-website-costs/',
+    toPath: '/meta/2024-my-website-costs/',
+    isPermanent: true,
+    redirectInBrowser: true
+  })
+}
