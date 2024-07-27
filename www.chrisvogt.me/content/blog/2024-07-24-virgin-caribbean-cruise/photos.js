33caf8adbce7ec1f9c90bf944a97e18cb8380527
Chris Vogt <mail@chrisvogt.me>
2024-07-27 14:13:58 -0700

    content: add a new blog post for my 2024 Virgin cruise

---
+++ www.chrisvogt.me/content/blog/2024-07-24-virgin-caribbean-cruise/photos.js
@@ -0,0 +1,305 @@
+export const planeFire = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722103976/chrisvogt-me/galleries/mayan-sol/1-plane-fire/20240712-IMG_9092.webp',
+    title: 'Cute dog sitting next to me on the plane.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722103979/chrisvogt-me/galleries/mayan-sol/1-plane-fire/20240712-CJV08985.webp',
+    title:
+      'AA 2045 on July 12, 2024. People in the back of the plane slid down the emergency slides to evacuate. Firefighters gathered around a burned backpack in the background.',
+    width: 3,
+    height: 2
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105385/chrisvogt-me/galleries/mayan-sol/1-plane-fire/20240712-023429DD-9A97-4EBB-BF02-B49D8E06C7E2_1_101_o.webp',
+    title: 'Screenshot of an ABC7 News video describing the plane evacuation.',
+    width: 23,
+    height: 50
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105883/chrisvogt-me/galleries/mayan-sol/1-plane-fire/20240712-27CAA9A7-A827-45D2-BD43-3CD1CE914DB2_1_101_o-2.webp',
+    title:
+      'Screenshot of the American Airlines Flight 2045 delayed screen. Originally scheduled to depart at 12:17PM, the next plane and crew wasn’t available until 11:15PM. Now a red eye flight to Miami.',
+    width: 1,
+    height: 1
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105385/chrisvogt-me/galleries/mayan-sol/1-plane-fire/20240713-IMG_9117.webp',
+    title: 'A news crew films passengers arriving at the baggage claim area in MIA.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const southBeachMiami = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105415/chrisvogt-me/galleries/mayan-sol/2-south-beach/20240713-IMG_9122.webp',
+    title: 'The top of a church in South Beach, Miami.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105413/chrisvogt-me/galleries/mayan-sol/2-south-beach/20240713-IMG_9129.webp',
+    title: 'A view of South Beach, Miami.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105414/chrisvogt-me/galleries/mayan-sol/2-south-beach/20240713-IMG_9143.webp',
+    title: 'Delicious food at CVI.CHE 105, a Peruvian restaurant in Miami.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105415/chrisvogt-me/galleries/mayan-sol/2-south-beach/20240713-IMG_9145.webp',
+    title: 'CVI.CHE 105, Miami.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const settingSailRoom = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105478/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240714-IMG_9172.webp',
+    title: 'Room 11 350Z became my home for the week.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105479/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240714-IMG_9173.webp',
+    title: 'Nick scopes out the view from our terrace with the Virgin port in the background.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105480/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240715-IMG_9275.webp',
+    title: 'My door magnets, a tradition on cruises, although I didn’t see many others with them this sailing.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const settingSailShip = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105480/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240714-IMG_9190.webp',
+    title:
+      'This was my favorite hot tub on the ship. On the top deck at the back. As we sailed away the Miami Skyline was behind it.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105481/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240715-IMG_9270.webp',
+    title:
+      'OK, so the pools were small, but they were still fun. Every day a crowd like this would gather and sozialize in the pool.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105480/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240714-IMG_9180.webp',
+    title: 'Sailing away from Miami on Valiant Lady, July 14, 2024.',
+    width: 4,
+    height: 3
+  }
+]
+
+export const settingSailLeftAtSea = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105478/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240716-IMG_9308.webp',
+    title: 'My watch alerting me that my laptop was last seen in the Caribbean Sea.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105478/chrisvogt-me/galleries/mayan-sol/3-cruise-life/20240715-IMG_9304.webp',
+    title: 'A screenshot of the Maps app showing my location in the Caribbean Sea.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const costaMaya = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105553/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9314.webp',
+    title:
+      'Costa Maya, Mexico from the cruise ship. This is a small port city that exists for the cruise ships, and Mahahual is 1.9mi south.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105553/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9313.webp',
+    title: 'Maya Lost Mayan Kingdom Adventure Park in the distance, taken from the Virgin Valiant Lady balcony.',
+    width: 4,
+    height: 3
+  }
+]
+
+export const costaMayaShip = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105548/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9323.webp',
+    title: 'The Virgin Valiant Lady docked in Costa Maya, Mexico.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105544/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9324.webp',
+    title: 'Nick standing in front of the Virgin Valiant Lady in Costa Maya, Mexico.',
+    width: 4,
+    height: 3
+  }
+]
+
+export const costaMayaMahahual = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105568/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9363.webp',
+    title: 'A mural in Mahahual, Mexico.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105571/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9365.webp',
+    title: 'A mural in Mahahual, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105560/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9362.webp',
+    title: 'A tree wrapped in colorful art in Mahahual, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105568/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9349.webp',
+    title: 'A van with a man painted on the front in Mahahual, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105557/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9345.webp',
+    title: 'Elephants kissing at the entrance to a hotel in Mahahual, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105560/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9355.webp',
+    title: 'A vendor on a bicycle in Mahahual, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105562/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9352.webp',
+    title: 'Looking down the malecón in Mahahual, Mexico.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const wildlifeInCostaMaya = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105558/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9342.webp',
+    title: 'Flamingos in Costa Maya, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105561/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9334.webp',
+    title: 'A blue-and-yellow macaw in Costa Maya, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105551/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_9401.webp',
+    title: 'A squirrel monkey perches on Nick’s shoulder in Costa Maya, Mexico.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105551/chrisvogt-me/galleries/mayan-sol/4-costa-maya/20240716-IMG_0711.webp',
+    title: 'If it fits, it sits. Squirrel monkey on my head in Costa Maya, Mexico.',
+    width: 4,
+    height: 3
+  }
+]
+
+export const scarletNight = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105653/chrisvogt-me/galleries/mayan-sol/5-day-at-sea/20240716-IMG_9471.webp',
+    title: 'An octopus set up on the pool deck for Scarlet Night.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105652/chrisvogt-me/galleries/mayan-sol/5-day-at-sea/20240716-IMG_9479.webp',
+    title: 'Dancing on the pool deck for Scarlet Night.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const welcomeToBimini = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105692/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9499.webp',
+    title: 'Welcome to Bimini sign in Bimini, Bahamas.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105689/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9503.webp',
+    title: 'The beach at Bimini, Bahamas.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105689/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9570.webp',
+    title: 'The Beach Club at Bimini, Bahamas.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105697/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9567.webp',
+    title: 'The Beach Club at Bimini, Bahamas.',
+    width: 3,
+    height: 4
+  }
+]
+
+export const biminiStingrays = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105691/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9557.webp',
+    title:
+      'We did a stingray excursion in Bimini, Bahamas and they boated us out to a private island surrounded by stingrays.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105691/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9562.webp',
+    title: 'The SS Sapona shipwreck in Bimini, Bahamas.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105699/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9511.webp',
+    title: 'Nick standing in the water with stingrays in Bimini, Bahamas.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105695/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9526.webp',
+    title: 'Nick holds his shirt out of the water as stingrays swim up to feel him in Bimini, Bahamas.',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105690/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9535.webp',
+    title: 'The stingrays swim up and brush their wings against you.',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1722105693/chrisvogt-me/galleries/mayan-sol/6-bahamas/20240718-IMG_9530.webp',
+    title: 'Nick brushes against a stingray in Bimini, Bahamas.',
+    width: 3,
+    height: 4
+  }
+]
