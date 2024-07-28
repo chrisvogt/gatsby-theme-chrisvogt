7df9290e82d5a5d20294e89785b80665645513d7
Chris Vogt <mail@chrisvogt.me>
2024-07-27 19:17:16 -0700

    SQUASHME: remove excerpts from photography and music

--- www.chrisvogt.me/src/pages/music.js
+++ www.chrisvogt.me/src/pages/music.js
@@ -40,7 +40,6 @@ const MusicPage = ({ data }) => {
               <PostCard
                 category={post.fields.category}
                 date={post.frontmatter.date}
-                excerpt={post.excerpt}
                 key={post.fields.id}
                 link={post.fields.path}
                 title={post.frontmatter.title}
@@ -68,7 +67,6 @@ export const pageQuery = graphql`
     allMdx(sort: { frontmatter: { date: DESC } }) {
       edges {
         node {
-          excerpt(pruneLength: 255)
           fields {
             category
             id
