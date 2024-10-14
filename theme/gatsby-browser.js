710bfdbddf9002820fec44c08b369c493a967810
Chris Vogt <mail@chrisvogt.me>
2024-10-13 18:28:44 -0700

    chore: [WIP] switch to pnpm

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -5,18 +5,17 @@ import './src/styles/global.css'
 
 import React from 'react'
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key='sidekickai-config'
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.__sidekickai = { key: '${process.env.GATSBY_SIDEKICK_API_KEY}' }
-        `,
-      }}
-    />,
-  ]);
-};
-
+// export const onRenderBody = ({ setHeadComponents }) => {
+//   setHeadComponents([
+//     <script
+//       key='sidekickai-config'
+//       dangerouslySetInnerHTML={{
+//         __html: `
+//           window.__sidekickai = { key: '${process.env.GATSBY_SIDEKICK_API_KEY}' }
+//         `,
+//       }}
+//     />,
+//   ]);
+// };
 
 export { default as wrapRootElement } from './wrapRootElement'
--- theme/gatsby-ssr.js
+++ theme/gatsby-ssr.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export const onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key='sidekickai-config'
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.__sidekickai = { key: '${process.env.GATSBY_SIDEKICK_API_KEY}' };
-        `,
-      }}
-    />,
-  ]);
-};
+// export const onInitialClientRender = ({ setHeadComponents }) => {
+//   setHeadComponents([
+//     <script
+//       key='sidekickai-config'
+//       dangerouslySetInnerHTML={{
+//         __html: `
+//           window.__sidekickai = { key: '${process.env.GATSBY_SIDEKICK_API_KEY}' };
+//         `,
+//       }}
+//     />,
+//   ]);
+// };
 
 export { default as wrapRootElement } from './wrapRootElement'
