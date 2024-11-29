961138ad1feb8ebe420aab1ffa2df1deba53eb65
Chris Vogt <mail@chrisvogt.me>
2024-11-28 21:42:51 -0800

    SQUASHME: downgrade chalk

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const boxen = require('boxen');
+const boxen = require('boxen')
+const chalk = require('chalk')
 
-(async () => {
-  const chalk = (await import('chalk')).default;
-  const package = require('./theme/package.json');
+const package = require('./theme/package.json')
 
-  const banner = boxen('www.chrisvogt.me\nMy Personal Website', {
-    align: 'center',
-    backgroundColor: '#9b20dc',
-    color: 'white',
-    padding: {
-      top: 1,
-      right: 8,
-      bottom: 1,
-      left: 8,
-    },
-  });
+const banner = boxen('www.chrisvogt.me\nMy Personal Website', {
+  align: 'center',
+  backgroundColor: '#9b20dc',
+  color: 'white',
+  padding: {
+    top: 1,
+    right: 8,
+    bottom: 1,
+    left: 8
+  }
+})
 
-  console.log(banner);
+console.log(banner)
 
-  console.log(`✅  Installation succeeded: ${chalk.bold('gatsby-theme-chrisvogt')}`);
-  console.log(`⚙  Version: ${chalk.bold(package.version)}`);
-})();
+console.log(`✅  Installation succeeded: ${chalk.bold('gatsby-theme-chrisvogt')}`)
+console.log(`⚙  Version: ${chalk.bold(package.version)}`)
