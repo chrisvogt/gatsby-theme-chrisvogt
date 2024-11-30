6a2681d7d27a6453d9936e34cd2655c622cae41e
Chris Vogt <mail@chrisvogt.me>
2024-11-30 12:41:18 -0800

    chore: add Emotion cache and move styles to head

--- theme/wrapRootElement.js
+++ theme/wrapRootElement.js
@@ -1,9 +1,10 @@
 /** @jsx jsx */
-import { Global } from '@emotion/react'
+import { Global, CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
 import { jsx } from 'theme-ui'
-import { MDXProvider, useMDXComponents } from '@mdx-js/react'
+import { MDXProvider } from '@mdx-js/react'
 import { Provider as ReduxProvider } from 'react-redux'
-import { Themed, useThemedStylesWithMdx } from '@theme-ui/mdx'
+import { Themed } from '@theme-ui/mdx'
 import { ThemeUIProvider } from 'theme-ui'
 
 import Emoji from './src/shortcodes/emoji'
@@ -11,30 +12,26 @@ import store from './src/store'
 import theme from './src/gatsby-plugin-theme-ui'
 import YouTube from './src/shortcodes/youtube'
 
+// Create an Emotion cache
+const cache = createCache({ key: 'css', prepend: true })
+
+// Define MDX components
 const components = {
   Emoji,
-  pre: ({ children }) => <Fragment>{children}</Fragment>,
+  pre: ({ children }) => <>{children}</>,
   YouTube,
   Table: (props) => <Themed.table {...props} sx={{ variant: 'styles.table' }} />
 }
 
-const WrapRootElement = ({ element }) => {
-  const componentsWithStyles = useThemedStylesWithMdx(
-    useMDXComponents(components)
-  )
-
-  return (
+const WrapRootElement = ({ element }) => (
+  <CacheProvider value={cache}>
     <ReduxProvider store={store}>
       <ThemeUIProvider theme={theme}>
         <Global styles={theme.global} />
-        <MDXProvider components={componentsWithStyles}>{element}</MDXProvider>
+        <MDXProvider components={components}>{element}</MDXProvider>
       </ThemeUIProvider>
     </ReduxProvider>
-  )
-}
-
-export const wrapRootElement = ({ element }) => (
-  <WrapRootElement element={element} />
+  </CacheProvider>
 )
 
-export default wrapRootElement
+export default WrapRootElement
