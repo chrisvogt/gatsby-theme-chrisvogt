249c71fd0dcc605b37ca516daf48658f30ec389c
Chris Vogt <mail@chrisvogt.me>
2024-12-08 02:44:11 -0800

    feat: factor out remove-react-images for lightgallery (#270)
    This PR swaps out **react-images** with
    [**lightgallery**](https://www.lightgalleryjs.com/). The former
    dependency has a README that says, "Don't use this in a new project.
    This package hasn't been properly maintained in a long time and there
    are much better options available."

--- theme/gatsby-browser.js
+++ theme/gatsby-browser.js
@@ -3,4 +3,9 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 
 import './src/styles/global.css'
 
+import 'lightgallery/css/lightgallery.css'
+import 'lightgallery/css/lg-thumbnail.css'
+import 'lightgallery/css/lg-zoom.css'
+import 'prismjs/themes/prism-solarizedlight.css'
+
 export { default as wrapRootElement } from './wrapRootElement'
