ad3369aae3a0a0914e582a1166cf8f9e99b89dc6
Chris Vogt <mail@chrisvogt.me>
2025-06-03 20:38:59 -0700

    SQUASHME: prevent resize on icon render

--- theme/src/components/widgets/widget-header.js
+++ theme/src/components/widgets/widget-header.js
@@ -17,7 +17,7 @@ const asideStyles = {
 const WidgetHeader = ({ aside, children, icon }) => (
   <header sx={headerStyles}>
     <Heading sx={{ fontSize: 5, display: 'flex', alignItems: 'center' }}>
-      {icon && <FontAwesomeIcon icon={icon} sx={{ fontSize: 4, mr: 2 }} />}
+      {icon && <FontAwesomeIcon icon={icon} sx={{ fontSize: 4, size: '24px', mr: 2 }} />}
       {children}
     </Heading>
     {aside && <div sx={asideStyles}>{aside}</div>}
