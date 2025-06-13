7af8bffefe831c83ee1f3de8f8f39bc618b9b1f7
Chris Vogt <mail@chrisvogt.me>
2025-06-12 23:03:36 -0700

    chore: organize postinstall script and convert to CommonJS

---
+++ scripts/postinstall-banner.js
@@ -0,0 +1,21 @@
+const boxen = require('boxen').default
+const chalk = require('chalk')
+const packageData = require('../theme/package.json')
+
+const banner = boxen(`${chalk.bold('www.chrisvogt.me')}\nMy Personal Website\nv${packageData.version}`, {
+  align: 'center',
+  backgroundColor: '#9b20dc',
+  borderStyle: 'round',
+  borderColor: '#7319a6',
+  color: 'white',
+  padding: {
+    top: 1,
+    right: 8,
+    bottom: 1,
+    left: 8
+  }
+})
+
+console.log(banner)
+
+console.log(`✅  Installation succeeded: ${chalk.bold('gatsby-theme-chrisvogt')}`)
