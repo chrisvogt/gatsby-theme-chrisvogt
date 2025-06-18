629c9f78df1e6b0d5eaf6e6030ac3c75c291f2da
Chris Vogt <mail@chrisvogt.me>
2025-06-18 16:04:59 -0700

    content: add blog post for April 25 cruise

---
+++ www.chrisvogt.me/content/blog/2025-06-18-virgin-southern-caribbean/photos.js
@@ -0,0 +1,443 @@
+export const flightToSanJuan = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231637/chrisvogt-me/galleries/2025-vsc/20250419-IMG_3558-2.jpg',
+    title: 'Nick looking out the plane, waiting to depart to San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231636/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2625.jpg',
+    title: 'Flying over clouds in the Caribbean',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231644/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2643.jpg',
+    title: 'Landing in San Juan, Puerto Rico',
+    width: 3,
+    height: 4
+  }
+]
+
+export const exploringOldSanJuanBefore = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231638/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2680.jpg',
+    title: 'Nick looking out the plane, waiting to depart to San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231670/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2669.jpg',
+    title: 'Old San Juan at night from a rooftop bar',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231682/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2666.jpg',
+    title: 'A cat in front of the ¡Boricua! sign alng the waterfront in Old San Juan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231649/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2655.jpg',
+    title: 'Mofongo at Hotel Rumbao in Old San Juan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231646/chrisvogt-me/galleries/2025-vsc/20250328-IMG_2662.jpg',
+    title: 'A door with an awning that says "Coqui Forest" in Old San Juan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231651/chrisvogt-me/galleries/2025-vsc/20250329-CJV09177.jpg',
+    title: 'A colorful building in Old San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231640/chrisvogt-me/galleries/2025-vsc/20250329-CJV09169.jpg',
+    title: 'Fort in Old San Juan',
+    width: 3,
+    height: 4
+  }
+]
+
+export const architectureInSanJuan = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231677/chrisvogt-me/galleries/2025-vsc/20250329-IMG_2723.jpg',
+    title: 'A colorful door in Old San Juan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231640/chrisvogt-me/galleries/2025-vsc/20250329-IMG_2718.jpg',
+    title: 'Building',
+    width: 4,
+    height: 3
+  }
+]
+
+export const streetPhotographyInSanJuan = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231694/chrisvogt-me/galleries/2025-vsc/20250329-IMG_2745.jpg',
+    title: 'Old San Juan street',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231674/chrisvogt-me/galleries/2025-vsc/20250329-IMG_2746.jpg',
+    title: 'People in the street',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231639/chrisvogt-me/galleries/2025-vsc/20250329-IMG_2741.jpg',
+    title: 'Nick smiling',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231654/chrisvogt-me/galleries/2025-vsc/20250329-IMG_2742.jpg',
+    title: 'Nick tongue',
+    width: 4,
+    height: 3
+  }
+]
+
+export const friendsArrivedInSanJuan = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231679/chrisvogt-me/galleries/2025-vsc/20250331-IMG_2752.jpg',
+    title: 'Group exploring',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231647/chrisvogt-me/galleries/2025-vsc/20250331-IMG_2756.jpg',
+    title: 'Matt sitting',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231673/chrisvogt-me/galleries/2025-vsc/20250331-IMG_4386.jpg',
+    title: 'Group eating',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231662/chrisvogt-me/galleries/2025-vsc/20250331-IMG_2763.jpg',
+    title: 'Chicken and rice at a restaurant',
+    width: 4,
+    height: 3
+  }
+]
+
+export const settingSail = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231667/chrisvogt-me/galleries/2025-vsc/20250401-IMG_2796.jpg',
+    title: 'Disembarking from the port in San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231652/chrisvogt-me/galleries/2025-vsc/20250401-IMG_2799.jpg',
+    title: 'The ship docked in San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231637/chrisvogt-me/galleries/2025-vsc/20250401-IMG_2811.jpg',
+    title: 'Celebrating setting sail',
+    width: 3,
+    height: 4
+  }
+]
+
+export const earlyShipDays = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231645/chrisvogt-me/galleries/2025-vsc/20250403-IMG_2861.jpg',
+    title: 'Group trying on glasses',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231647/chrisvogt-me/galleries/2025-vsc/20250403-IMG_2835.jpg',
+    title: 'Group at the pool on the ship',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231689/chrisvogt-me/galleries/2025-vsc/20250403-IMG_2868.jpg',
+    title: 'Goofy times at the carousel',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231648/chrisvogt-me/galleries/2025-vsc/20250402-IMG_2828.jpg',
+    title: 'Squad walking down the stairs',
+    width: 3,
+    height: 4
+  }
+]
+
+export const eatingAtGunbae = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231655/chrisvogt-me/galleries/2025-vsc/20250402-IMG_2831.jpg',
+    title: 'Eating at Gunbae, a Korean BBQ restaurant on the ship, with Krish',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231687/chrisvogt-me/galleries/2025-vsc/20250402-IMG_2830.jpg',
+    title: 'The food at Gunbae',
+    width: 4,
+    height: 3
+  }
+]
+
+export const birdsOfCartagena = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231690/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2883.jpg',
+    title: 'Birds in the port of Cartagena, Colombia',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231648/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2895.jpg',
+    title: 'Birds in the port of Cartagena, Colombia',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231699/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2907.jpg',
+    title: 'Peacock',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231649/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2912.jpg',
+    title: 'Tucan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231691/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2915.jpg',
+    title: 'Unknown bird',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231673/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2918.jpg',
+    title: 'Baby flamingo',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231691/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2922.jpg',
+    title: 'Pair of birds',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231659/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2924.jpg',
+    title: 'Weird bird',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231691/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2926.jpg',
+    title: 'Fluffy parrot',
+    width: 3,
+    height: 4
+  }
+]
+
+export const cartagenaStreets = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231668/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2939.jpg',
+    title: 'Street in Cartagena, Colombia',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231676/chrisvogt-me/galleries/2025-vsc/20250404-IMG_3007.jpg',
+    title: 'Street with shadows',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231685/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2946.jpg',
+    title: 'Old cars in the street',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231664/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2949.jpg',
+    title: 'Flag at the port',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231686/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2979.jpg',
+    title: 'Wine door',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231680/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2977.jpg',
+    title: 'Umbrella street',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231665/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2942.jpg',
+    title: 'Street in Cartagena, Colombia',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231646/chrisvogt-me/galleries/2025-vsc/20250404-IMG_3033.jpg',
+    title: 'Street in Cartagena, Colombia at night',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231696/chrisvogt-me/galleries/2025-vsc/20250404-IMG_3037.jpg',
+    title: 'Stained glass windows in a church',
+    width: 3,
+    height: 4
+  }
+]
+
+export const cartagenaRooftops = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231637/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2994.jpg',
+    title: 'Rooftop view with church',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231691/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2997.jpg',
+    title: 'Rooftop view looking down',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231666/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2987.jpg',
+    title: 'Looking out',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231673/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2965.jpg',
+    title: 'Another rooftop',
+    width: 4,
+    height: 3
+  }
+]
+
+export const cartagenaFriends = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231644/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2992.jpg',
+    title: 'Nick and Eric',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231647/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2991.jpg',
+    title: 'Matt sitting',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231657/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2960.jpg',
+    title: 'Happy, hungry Matt',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231659/chrisvogt-me/galleries/2025-vsc/20250404-IMG_2962.jpg',
+    title: 'Blue crab nachos',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231639/chrisvogt-me/galleries/2025-vsc/20250404-IMG_3002.jpg',
+    title: 'Eric',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231636/chrisvogt-me/galleries/2025-vsc/20250404-IMG_3028.jpg',
+    title: 'Chris and Nick',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231652/chrisvogt-me/galleries/2025-vsc/20250404-IMG_3026.jpg',
+    title: 'Food',
+    width: 4,
+    height: 3
+  }
+]
+
+export const curacaoFriends = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231636/chrisvogt-me/galleries/2025-vsc/20250407-CJV09225.jpg',
+    title: 'Matt',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231638/chrisvogt-me/galleries/2025-vsc/20250407-CJV09220.jpg',
+    title: 'Eric',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231636/chrisvogt-me/galleries/2025-vsc/20250407-CJV09229.jpg',
+    title: 'Nick',
+    width: 16,
+    height: 9
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231659/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3130.jpg',
+    title: 'Friends crossing the bridge',
+    width: 3,
+    height: 4
+  }
+]
+
+export const curacaoStreets = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231642/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3127.jpg',
+    title: 'From the bridge',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231654/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3138.jpg',
+    title: 'Up close',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231656/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3123.jpg',
+    title: 'Types of curacao',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231696/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3131.jpg',
+    title: 'Mural in the city',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231679/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3132.jpg',
+    title: 'Dutch newspaper stand',
+    width: 3,
+    height: 4
+  }
+]
