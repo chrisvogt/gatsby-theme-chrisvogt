45eb26d03a79f0b87f583953b565b8ac06514f8c
Chris Vogt <mail@chrisvogt.me>
2025-06-18 17:35:05 -0700

    SQUASHME: continue adding photos

--- www.chrisvogt.me/content/blog/2025-06-18-virgin-southern-caribbean/photos.js
+++ www.chrisvogt.me/content/blog/2025-06-18-virgin-southern-caribbean/photos.js
@@ -441,3 +441,210 @@ export const curacaoStreets = [
     height: 4
   }
 ]
+
+export const scarletNightAndAtSea = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231685/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3153.jpg',
+    title: 'Nick getting his face painted at Scarlet Night',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231640/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3162.jpg',
+    title: 'Nick with his face painted',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231665/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3167.jpg',
+    title: 'Matt on Scarlet Night',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231653/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3173.jpg',
+    title: 'Chris on Scarlet Night',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231694/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3184.jpg',
+    title: 'The octopus on the ship',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231638/chrisvogt-me/galleries/2025-vsc/20250407-IMG_3189.jpg',
+    title: 'The ship at night',
+    width: 3,
+    height: 4
+  }
+]
+
+export const atSea = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231653/chrisvogt-me/galleries/2025-vsc/20250409-IMG_8484.jpg',
+    title: 'Chris and Nick in the casino',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231660/chrisvogt-me/galleries/2025-vsc/20250409-IMG_3248.jpg',
+    title: 'Chris and Nick at the casino',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231644/chrisvogt-me/galleries/2025-vsc/20250409-IMG_3274.jpg',
+    title: 'Matt putting feathers in his hair',
+    width: 3,
+    height: 4
+  }
+]
+
+export const antiguaFirstSpot = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231668/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3282.jpg',
+    title: 'Nick looking at a lizard',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231653/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3287.jpg',
+    title: 'Lizard up close',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231641/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3290.jpg',
+    title: 'Docked catamaran',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231682/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3292.jpg',
+    title: 'Pirate-themed bar',
+    width: 3,
+    height: 4
+  }
+]
+
+export const antiguaFloatingBar = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231645/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3295.jpg',
+    title: 'Riding a boat over to the floating bar',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231680/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3301.jpg',
+    title: 'Floating bar',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231657/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3321.jpg',
+    title: 'Kon Tiki, the floating bar',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231650/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3308.jpg',
+    title: "Nick doesn't want to leave, ever",
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231644/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3350.jpg',
+    title: 'Looking inland',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231636/chrisvogt-me/galleries/2025-vsc/20250410-IMG_3359.jpg',
+    title: 'Sunset at the floating bar',
+    width: 4,
+    height: 3
+  }
+]
+
+export const stMaarten = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231676/chrisvogt-me/galleries/2025-vsc/20250411-IMG_3390.jpg',
+    title: 'The gang in St. Maarten',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231641/chrisvogt-me/galleries/2025-vsc/20250411-IMG_3409.jpg',
+    title: 'Nick on the boat in St. Maarten',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231671/chrisvogt-me/galleries/2025-vsc/20250411-IMG_3426.jpg',
+    title: 'Port of St Maarten',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231664/chrisvogt-me/galleries/2025-vsc/20250411-IMG_3412.jpg',
+    title: 'The cruise ship in St. Maarten',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231682/chrisvogt-me/galleries/2025-vsc/20250411-IMG_3381.jpg',
+    title: 'Cocktails menu at the bar',
+    width: 3,
+    height: 4
+  }
+]
+
+export const finalSanJuan = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231642/chrisvogt-me/galleries/2025-vsc/20250418-IMG_3522.jpg',
+    title: 'Ocean Beach in San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231652/chrisvogt-me/galleries/2025-vsc/20250413-IMG_3449.jpg',
+    title: 'Ocean Beach in San Juan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231688/chrisvogt-me/galleries/2025-vsc/20250413-IMG_3451.jpg',
+    title: 'Cats in San Juan',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231662/chrisvogt-me/galleries/2025-vsc/20250412-IMG_3446.jpg',
+    title: 'BBQ in San Juan',
+    width: 4,
+    height: 3
+  }
+]
+
+export const friendsWeMadeInSanJuan = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231668/chrisvogt-me/galleries/2025-vsc/20250413-IMG_3477.jpg',
+    title: 'Chris, Tiff and Nick at Condado Beach in San Juan',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231670/chrisvogt-me/galleries/2025-vsc/20250413-IMG_3474.jpg',
+    title: 'Dancing to disco on Condado Beach while the sun sets',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/v1750231685/chrisvogt-me/galleries/2025-vsc/20250418-IMG_3536.jpg',
+    title: 'Jose and Nick on the patio',
+    width: 4,
+    height: 3
+  }
+]
