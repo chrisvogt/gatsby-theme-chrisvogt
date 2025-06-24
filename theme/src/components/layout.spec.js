55e2fa921e188e6af341bc1e573067a255c7aad1
Chris Vogt <mail@chrisvogt.me>
2025-06-23 20:43:23 -0700

    SQUASHME: improve code coverage

--- theme/src/components/layout.spec.js
+++ theme/src/components/layout.spec.js
@@ -57,4 +57,23 @@ describe('Layout', () => {
 
     expect(tree).toMatchSnapshot()
   })
+
+  it('renders children without main wrapper when disableMainWrapper is true', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <ThemeUIProvider theme={mockTheme}>
+            <Layout disableMainWrapper={true}>
+              <div className='fake-website'>
+                <h1>Fake Website</h1>
+                <p>Lorum ipsum dolor sit amet.</p>
+              </div>
+            </Layout>
+          </ThemeUIProvider>
+        </Provider>
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
 })
