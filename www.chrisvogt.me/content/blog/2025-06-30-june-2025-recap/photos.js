186f3b4a40ab53a5135b7e68f5faa21b6aad5b08
Chris Vogt <mail@chrisvogt.me>
2025-07-01 09:03:32 -0700

    feat: extend the recent Now page post with photos

--- www.chrisvogt.me/content/blog/2025-06-30-june-2025-recap/photos.js
+++ www.chrisvogt.me/content/blog/2025-06-30-june-2025-recap/photos.js
@@ -1,75 +1,93 @@
 export const pridePhotos = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355494/chrisvogt-me/galleries/now-june-2025/castro-on-pride-2025.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355481/chrisvogt-me/galleries/now-june-2025/castro-art-walk.avif',
+    title: 'Castro Art Walk',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355494/chrisvogt-me/galleries/now-june-2025/castro-on-pride-2025.avif',
     title: 'Castro St on Pride 2025',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355488/chrisvogt-me/galleries/now-june-2025/pride-2025-dolores-park.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355488/chrisvogt-me/galleries/now-june-2025/pride-2025-dolores-park.avif',
     title: 'Pride 2025 at Dolores Park',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355491/chrisvogt-me/galleries/now-june-2025/mat-at-the-mix.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355491/chrisvogt-me/galleries/now-june-2025/pride-dolo.avif',
+    title: 'Dolores Park on Pride weekend',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355491/chrisvogt-me/galleries/now-june-2025/mat-at-the-mix.avif',
     title: 'Mat at the Mix',
     width: 4,
     height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355470/chrisvogt-me/galleries/now-june-2025/party-performance.avif',
+    title: 'Adam performing a Coming Out improv scene at his Pride party',
+    width: 3,
+    height: 4
   }
 ]
 
 export const vinylCollection = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355465/chrisvogt-me/galleries/now-june-2025/love-letters-in-latin-cover.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355465/chrisvogt-me/galleries/now-june-2025/love-letters-in-latin-cover.avif',
     title: "It all started with Love Letters in Latin, my grandfather's vinyl",
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355466/chrisvogt-me/galleries/now-june-2025/love-letters-in-lattin-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355466/chrisvogt-me/galleries/now-june-2025/love-letters-in-lattin-vinyl.avif',
     title: 'Love Letters in Latin up close',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355483/chrisvogt-me/galleries/now-june-2025/mutemath-play-dead-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355483/chrisvogt-me/galleries/now-june-2025/mutemath-play-dead-vinyl.avif',
     title: 'Mutemath, Play Dead',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355451/chrisvogt-me/galleries/now-june-2025/daft-punk-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355451/chrisvogt-me/galleries/now-june-2025/daft-punk-vinyl.avif',
     title: 'Random Access Memories by Daft Punk, my favorite album',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355471/chrisvogt-me/galleries/now-june-2025/chappell-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355471/chrisvogt-me/galleries/now-june-2025/chappell-vinyl.avif',
     title: 'Chappell Roan, Rise and Fall of a Midwest Princess',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355476/chrisvogt-me/galleries/now-june-2025/lady-gaga-mayhem-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355476/chrisvogt-me/galleries/now-june-2025/lady-gaga-mayhem-vinyl.avif',
     title: 'Lady Gaga, Mayhem',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355478/chrisvogt-me/galleries/now-june-2025/mutemath-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355478/chrisvogt-me/galleries/now-june-2025/mutemath-vinyl.avif',
     title: 'Mutemath LP',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355477/chrisvogt-me/galleries/now-june-2025/linkin-park-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355477/chrisvogt-me/galleries/now-june-2025/linkin-park-vinyl.avif',
     title: 'Linkin Park, Minutes to Midnight',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355480/chrisvogt-me/galleries/now-june-2025/finneas-vinyl.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355480/chrisvogt-me/galleries/now-june-2025/finneas-vinyl.avif',
     title: "FINNEAS, For Cryin' Out Loud",
     width: 3,
     height: 4
@@ -78,34 +96,112 @@ export const vinylCollection = [
 
 export const brewing = [
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355452/chrisvogt-me/galleries/now-june-2025/prepping-to-brew.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355452/chrisvogt-me/galleries/now-june-2025/prepping-to-brew.avif',
     title: 'Preparing to brew',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355453/chrisvogt-me/galleries/now-june-2025/prepping-to-brew-pot.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355453/chrisvogt-me/galleries/now-june-2025/prepping-to-brew-pot.avif',
     title: 'Brewing on a stovetop induction burner',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355456/chrisvogt-me/galleries/now-june-2025/brewing-hops.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355456/chrisvogt-me/galleries/now-june-2025/brewing-hops.avif',
     title: 'Brewing stage: adding hops',
     width: 4,
     height: 3
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355457/chrisvogt-me/galleries/now-june-2025/carboy-fermenting.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355457/chrisvogt-me/galleries/now-june-2025/carboy-fermenting.avif',
     title: 'Fermenting in a carboy',
     width: 3,
     height: 4
   },
   {
-    src: 'https://res.cloudinary.com/chrisvogt/image/upload/f_auto/v1751355456/chrisvogt-me/galleries/now-june-2025/brewing-bottled.avif',
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355456/chrisvogt-me/galleries/now-june-2025/brewing-bottled.avif',
     title: 'Bottling the beer',
     width: 4,
     height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355485/chrisvogt-me/galleries/now-june-2025/beer-up-close.avif',
+    title: 'Beer, up close',
+    width: 3,
+    height: 4
+  }
+]
+
+export const gardening = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355463/chrisvogt-me/galleries/now-june-2025/close-up-gardening.avif',
+    title: 'Close up of the AeroGarden in my kitchen',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355488/chrisvogt-me/galleries/now-june-2025/propagating-up-close.avif',
+    title: 'Propagating plants',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355487/chrisvogt-me/galleries/now-june-2025/propagating-plants.avif',
+    title: 'Propagating my monstera deliciosa',
+    width: 4,
+    height: 3
+  }
+]
+
+export const favoritePhotos = [
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355485/chrisvogt-me/galleries/now-june-2025/mission-mural.avif',
+    title: 'A mural in the Mission',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355473/chrisvogt-me/galleries/now-june-2025/no-kings-protest-castro-sf-1.avif',
+    title: 'No Kings protest in Castro, SF',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355474/chrisvogt-me/galleries/now-june-2025/no-kings-protest-castro-flag.avif',
+    title: 'Young girl holding a USA flag at the No Kings protest in Castro, SF',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355470/chrisvogt-me/galleries/now-june-2025/no-kings-protest-castro-sf.avif',
+    title: 'People marching in the No Kings protest in Castro, SF',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355468/chrisvogt-me/galleries/now-june-2025/twin-peaks-balcony.avif',
+    title: 'Looking downtown from a Twin Peaks balcony',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355458/chrisvogt-me/galleries/now-june-2025/which-birb-are-you.avif',
+    title: 'Which birb are you?',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload//c_scale,h_900,f_auto/v1751355462/chrisvogt-me/galleries/now-june-2025/my-bird.avif',
+    title: 'I am a mallard duck',
+    width: 3,
+    height: 4
+  },
+  {
+    src: 'https://res.cloudinary.com/chrisvogt/image/upload/c_scale,h_900,f_auto/v1751355455/chrisvogt-me/galleries/now-june-2025/hopscotch-alejandro.avif',
+    title: 'Alejandro and Roc play hopscotch in the Mission',
+    width: 3,
+    height: 4
   }
 ]
 
